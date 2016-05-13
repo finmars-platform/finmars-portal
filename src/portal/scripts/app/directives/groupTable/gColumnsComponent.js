5a67b81f7ba9ebdf9be0ba05ef8a9474791d5107
Sergei Zhitenev <s.zhitenev@finmars.com>
2016-05-13 10:57:54 +0300

    drag-drop updates

--- src/portal/scripts/app/directives/groupTable/gColumnsComponent.js
+++ src/portal/scripts/app/directives/groupTable/gColumnsComponent.js
@@ -1,32 +1,32 @@
 /**
  * Created by szhitenev on 05.05.2016.
  */
-(function(){
+(function () {
 
     'use strict';
 
-    module.exports = function() {
+    module.exports = function () {
         return {
             restrict: 'AE',
             scope: {
                 columns: '='
             },
             templateUrl: 'views/directives/groupTable/columns-view.html',
-            link: function(scope, elem, attrs) {
+            link: function (scope, elem, attrs) {
                 console.log('Columns component');
 
-                scope.openColumnSettings = function($mdOpenMenu, ev) {
+                scope.openColumnSettings = function ($mdOpenMenu, ev) {
                     $mdOpenMenu(ev);
                 };
 
-                scope.removeColumn = function(column) {
+                scope.removeColumn = function (column) {
                     console.log('remove', column);
-                    scope.columns = scope.columns.map(function(item){
-                        if(item === column) {
+                    scope.columns = scope.columns.map(function (item) {
+                        if (item === column) {
                             return undefined
                         }
                         return item
-                    }).filter(function(item){
+                    }).filter(function (item) {
                         return !!item;
                     })
                 }
