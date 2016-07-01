0d5c61b012c235e14285ec536a7c648881c514ba
Sergei Zhitenev <s.zhitenev@finmars.com>
2016-07-01 13:08:18 +0300

    datepicker

---
+++ src/portal/scripts/app/directives/zhDatePickerDirective.js
@@ -0,0 +1,43 @@
+/**
+ * Created by szhitenev on 01.07.2016.
+ */
+(function () {
+
+    'use strict';
+
+    module.exports = function () {
+
+        return {
+            restrict: 'AE',
+            scope: {},
+            require: '?ngModel',
+            template: '<div class="pick-me-up"><input type="text" value=""></div>',
+            link: function (scope, elem, attrs, ngModel) {
+                console.log('ngModel', ngModel);
+                var input = $(elem).find('input');
+
+                scope.$watch(function () {
+                    console.log('ngModel', ngModel);
+                    if (!!ngModel.$modelValue) {
+                        input.val(moment(new Date(ngModel.$modelValue)).format('YYYY/MM/DD'));
+                    }
+                });
+
+                input.pickmeup({
+                    position: 'right',
+                    'hide_on_select': true,
+                    format: 'Y/m/d',
+                    change: function () {
+                        console.log(this);
+                        console.log(this.value);
+                        console.log('ngModel', ngModel);
+                        ngModel.$setViewValue(this.value);
+                    }
+                });
+
+            }
+        }
+
+    }
+
+}());
