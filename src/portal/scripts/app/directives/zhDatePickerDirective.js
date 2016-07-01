048c4d811e59db37900d9e0bba215a4956824ecd
Сергей Житенев < >
2016-07-01 17:10:59 +0300

    fixing

--- src/portal/scripts/app/directives/zhDatePickerDirective.js
+++ src/portal/scripts/app/directives/zhDatePickerDirective.js
@@ -13,11 +13,11 @@
             require: '?ngModel',
             template: '<div class="pick-me-up"><input type="text" value=""></div>',
             link: function (scope, elem, attrs, ngModel) {
-                console.log('ngModel', ngModel);
+                //console.log('ngModel', ngModel);
                 var input = $(elem).find('input');
 
                 scope.$watch(function () {
-                    console.log('ngModel', ngModel);
+                    //console.log('ngModel', ngModel);
                     if (!!ngModel.$modelValue) {
                         input.val(moment(new Date(ngModel.$modelValue)).format('YYYY/MM/DD'));
                     }
@@ -28,9 +28,9 @@
                     'hide_on_select': true,
                     format: 'Y/m/d',
                     change: function () {
-                        console.log(this);
-                        console.log(this.value);
-                        console.log('ngModel', ngModel);
+                        //console.log(this);
+                        //console.log(this.value);
+                        //console.log('ngModel', ngModel);
                         ngModel.$setViewValue(this.value);
                     }
                 });
