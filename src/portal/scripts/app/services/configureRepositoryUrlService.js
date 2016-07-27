27de6a99bc0cf2010f2272073268963e43a56a5d
DopingGood <nektomakseem@yandex.ru>
2016-07-27 11:44:13 +0300

    multiple choices

--- src/portal/scripts/app/services/configureRepositoryUrlService.js
+++ src/portal/scripts/app/services/configureRepositoryUrlService.js
@@ -3,47 +3,65 @@
  */
 (function () {
 
-    'use strict';
+	'use strict';
 
-    function configureUrl(url, options) {
-        if (options) {
-            if (options.sort) {
-                if (options.sort.direction === 'ASC') {
-                    url = url + '?ordering=' + options.sort.key
-                } else {
-                    url = url + '?ordering=-' + options.sort.key
-                }
-            }
-            var keys = [];
-            if (options.filters && options.sort) {
-                keys = Object.keys(options.filters);
-                keys.forEach(function (item) {
-                    if (options.filters[item]) {
-                        url = url + '&' + item + '=' + options.filters[item];
-                    }
-                })
-            }
-            if (options.filters && !options.sort) {
-                keys = Object.keys(options.filters);
-                keys.forEach(function (item, i) {
-                    if (i === 0) {
-                        console.log('options.filters[item]', options.filters[item]);
-                        url = url + '?' + item + '=' + options.filters[item];
-                    } else {
-                        url = url + '&' + item + '=' + options.filters[item];
-                    }
-                })
-            }
+	function configureUrl(url, options) {
+		if (options) {
+			if (options.filters && options.sort.key) {
+				keys = Object.keys(options.filters);
+				keys.forEach(function (keysItem) {
+					if (options.filters[keysItem]) {
+						var filterItems = options.filters[keysItem];
+						filterItems.map(function (filterItem, index) {
+							if (index === 0) {
+								url = url + '?' + keysItem + '=' + filterItem;
+							} else {
+								url = url + '&' + keysItem + '=' + filterItem;
+							} 
+						})
+					}
+				})
+			}
+			else if (options.filters && !options.sort.key) {
+				var keys = [];
+				keys = Object.keys(options.filters);
+				keys.forEach(function (keysItem, i) {
+					var filterItems = options.filters[keysItem];
+					// if (i === 0) {
+						// console.log('options.filters[keysItem]', filterItems);
+					filterItems.map(function (filterItem, index) {
+						if (index === 0) {
+							url = url + '?' + keysItem + '=' + filterItem;
+						} else {
+							url = url + '&' + keysItem + '=' + filterItem;
+						}
+					})
+					// } else {
+					// 	console.log("without sorting");
+					// 	filterItems.map(function (filterItem) {
+					// 		url = url + '&' + keysItem + '=' + filterItem;
+					// 	})
+					// }
+				})
+			}
+			else if (options.sort.key) {
+				console.log('yet sorting');
+				if (options.sort.direction === 'ASC') {
+					url = url + '?ordering=' + options.sort.key
+				} else {
+					url = url + '?ordering=-' + options.sort.key
+				}
+			}
 
-            if(options.page) {
-                url = url + '&page=' + options.page;
-            }
-        }
-        return url
-    }
+			if(options.page) {
+				url = url + '&page=' + options.page;
+			}
+		}
+		return url
+	}
 
-    module.exports = {
-        configureUrl: configureUrl
-    }
+	module.exports = {
+		configureUrl: configureUrl
+	}
 
 }());
