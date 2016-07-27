c909e31e5ed48a492927dcde538a41f69a8130f6
Sergei Zhitenev <s.zhitenev@finmars.com>
2016-07-27 13:52:00 +0300

    fixes

--- src/portal/scripts/app/services/configureRepositoryUrlService.js
+++ src/portal/scripts/app/services/configureRepositoryUrlService.js
@@ -7,55 +7,41 @@
 
 	function configureUrl(url, options) {
 		if (options) {
-			if (options.filters && options.sort.key) {
+
+			url = url + '?';
+
+			var keys = [];
+
+			if(options.hasOwnProperty('filters')) {
 				keys = Object.keys(options.filters);
 				keys.forEach(function (keysItem) {
 					if (options.filters[keysItem]) {
 						var filterItems = options.filters[keysItem];
-						filterItems.map(function (filterItem, index) {
-							if (index === 0) {
-								url = url + '?' + keysItem + '=' + filterItem;
-							} else {
+						if (typeof filterItems === 'string') {
+							url = url + '&' + keysItem + '=' + filterItems;
+						} else {
+							filterItems.map(function (filterItem, index) {
 								url = url + '&' + keysItem + '=' + filterItem;
-							} 
-						})
+							})
+						}
 					}
 				})
 			}
-			else if (options.filters && !options.sort.key) {
-				var keys = [];
-				keys = Object.keys(options.filters);
-				keys.forEach(function (keysItem, i) {
-					var filterItems = options.filters[keysItem];
-					// if (i === 0) {
-						// console.log('options.filters[keysItem]', filterItems);
-					filterItems.map(function (filterItem, index) {
-						if (index === 0) {
-							url = url + '?' + keysItem + '=' + filterItem;
-						} else {
-							url = url + '&' + keysItem + '=' + filterItem;
-						}
-					})
-					// } else {
-					// 	console.log("without sorting");
-					// 	filterItems.map(function (filterItem) {
-					// 		url = url + '&' + keysItem + '=' + filterItem;
-					// 	})
-					// }
-				})
-			}
-			else if (options.sort.key) {
-				console.log('yet sorting');
+
+			if(options.hasOwnProperty('sort')) {
 				if (options.sort.direction === 'ASC') {
-					url = url + '?ordering=' + options.sort.key
+					url = url + '&ordering=' + options.sort.key
 				} else {
-					url = url + '?ordering=-' + options.sort.key
+					url = url + '&ordering=-' + options.sort.key
 				}
 			}
 
-			if(options.page) {
+			if(options.hasOwnProperty('page')) {
 				url = url + '&page=' + options.page;
 			}
+
+
+			console.log('URL------------------', url);
 		}
 		return url
 	}
