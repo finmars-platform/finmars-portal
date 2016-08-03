50181f0eacf2ef6853254d0adc7bf1be23781e20
Sergei Zhitenev <s.zhitenev@finmars.com>
2016-08-03 16:05:38 +0300

    update

--- src/portal/scripts/app/controllers/settings/settingsGeneralController.js
+++ src/portal/scripts/app/controllers/settings/settingsGeneralController.js
@@ -7,9 +7,31 @@
 
     var logService = require('../../../../../core/services/logService');
 
+    var timeZonesService = require('../../services/timeZonesService');
+
+    var usersService = require('../../services/usersService');
+
     module.exports = function($scope) {
 
         logService.controller('SettingsGeneralController', 'initialized');
+
+        var vm = this;
+
+        vm.readyStatus = {user: false};
+
+        vm.timeZones = timeZonesService.getList();
+
+        usersService.getByKey(0).then(function(data){
+            vm.user = data;
+            vm.readyStatus.user = true;
+            $scope.$apply();
+        });
+
+        vm.save = function(){
+            usersService.update(0, vm.user).then(function(){
+                $scope.$apply();
+            })
+        }
 
     };
 
