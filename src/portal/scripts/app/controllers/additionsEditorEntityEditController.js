4bd93da2e0f3b09045fb76b33f001d16ec443eec
sergey <zhitenev36@gmail.com>
2016-09-06 18:11:35 +0300

    make a copy btn

--- src/portal/scripts/app/controllers/additionsEditorEntityEditController.js
+++ src/portal/scripts/app/controllers/additionsEditorEntityEditController.js
@@ -3,62 +3,89 @@
  */
 (function () {
 
-    'use strict';
+        'use strict';
 
-    var logService = require('../../../../core/services/logService');
+        var logService = require('../../../../core/services/logService');
 
-    var attributeTypeService = require('../services/attributeTypeService');
-    var entityResolverService = require('../services/entityResolverService');
+        var attributeTypeService = require('../services/attributeTypeService');
+        var entityResolverService = require('../services/entityResolverService');
 
-    var uiService = require('../services/uiService');
+        var uiService = require('../services/uiService');
 
-    var gridHelperService = require('../services/gridHelperService');
-    var metaService = require('../services/metaService');
-    var layoutService = require('../services/layoutService');
+        var gridHelperService = require('../services/gridHelperService');
+        var metaService = require('../services/metaService');
+        var layoutService = require('../services/layoutService');
 
 
-    // codemonkey
+        // codemonkey
 
-    module.exports = function ($scope, $state, $mdDialog) {
+        module.exports = function ($scope, $state, $mdDialog) {
 
-        logService.controller('AdditionsEditorEntityEditController', 'initialized');
+            logService.controller('AdditionsEditorEntityEditController', 'initialized');
 
-        //console.log('scope', $scope);
+            //console.log('scope', $scope);
 
-        var vm = this;
+            var vm = this;
 
-        vm.readyStatus = {content: false};
-        vm.entityType = $scope.$parent.entityType;
-        vm.entity = {attributes: []};
-        vm.entityId = '';
+            vm.readyStatus = {content: false};
+            vm.entityType = $scope.$parent.entityType;
+            vm.entity = {attributes: []};
+            vm.entityId = '';
+            vm.evAction = 'update';
 
-        vm.readyStatus.entityId = false;
-
-        $scope.$parent.$watch('itemId', function (newItemId) {
             vm.readyStatus.entityId = false;
-            setTimeout(function () {
-                vm.entityId = newItemId;
-                if (vm.entityId !== undefined) {
-                    vm.readyStatus.entityId = true;
+
+            $scope.$parent.$watch('itemId', function (newItemId) {
+                vm.readyStatus.entityId = false;
+                setTimeout(function () {
+                    vm.entityId = newItemId;
+                    if (vm.entityId !== undefined) {
+                        vm.evAction = 'update';
+                        vm.readyStatus.entityId = true;
+                    }
+                    $scope.$apply();
+                }, 100)
+
+            });
+
+            vm.editLayout = function () {
+                $state.go('app.data-constructor', {entityType: vm.entityType});
+            };
+
+            vm.manageAttrs = function (ev) {
+                $state.go('app.attributesManager', {entityType: vm.entityType});
+            };
+
+            vm.copy = function () {
+                vm.copyCallback().then(function () {
+                    vm.evAction = 'create';
+                    $scope.$apply();
+                }); // look at entityEditorController
+            };
+
+            vm.save = function () {
+
+                if (vm.evAction = 'create') {
+                    vm.saveCallback().then(function (options) {
+
+                        entityResolverService.create(options.entityType, options.entity).then(function () {
+                            vm.evAction = 'update';
+                            $scope.$apply();
+                        });
+
+                    })
+                } else {
+                    vm.saveCallback().then(function (options) {
+
+                        entityResolverService.update(options.entityType, options.entityId, options.entity).then(function () {
+                            $scope.$apply();
+                        });
+                    })
+
                 }
-                $scope.$apply();
-            }, 100)
 
-        });
+            }
+        }
 
-        vm.editLayout = function () {
-            $state.go('app.data-constructor', {entityType: vm.entityType});
-        };
-
-        vm.save = function () {
-            vm.saveCallback().then(function (options) {
-
-                entityResolverService.update(options.entityType, options.entityId, options.entity).then(function () {
-                    $mdDialog.hide({res: 'agree'});
-                });
-            })
-        };
-
-    }
-
-}());
+    }()
+);
