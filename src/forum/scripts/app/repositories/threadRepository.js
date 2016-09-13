0e984f809616a7bb2ee61a1d846fa8d409bddaa0
DopingGood <nektomakseem@yandex.ru>
2016-09-13 17:55:45 +0300

    forum tags directive

--- src/forum/scripts/app/repositories/threadRepository.js
+++ src/forum/scripts/app/repositories/threadRepository.js
@@ -3,98 +3,98 @@
  */
 (function(){
 
-    'use strict';
+	'use strict';
 
-    var cookieService = require('../../../../core/services/cookieService');
+	var cookieService = require('../../../../core/services/cookieService');
 
-    var baseUrl = '/api/v1/';
+	var baseUrl = '/api/v1/';
 
-    var getList = function (options) {
-        return window.fetch(baseUrl + 'chats/thread/?ordering=created&page=' + options.page + '&thread_group=' + options.threadGroup,
-            {
-                method: 'GET',
-                credentials: 'include',
-                headers: {
-                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
-                    Accept: 'application/json',
-                    'Content-type': 'application/json'
-                }
-            }).then(function (data) {
-            return data.json();
-        })
-    };
+	var getList = function (options) {
+		return window.fetch(baseUrl + 'chats/thread/?ordering=created&page=' + options.page + '&thread_group=' + options.threadGroup,
+			{
+				method: 'GET',
+				credentials: 'include',
+				headers: {
+					'X-CSRFToken': cookieService.getCookie('csrftoken'),
+					Accept: 'application/json',
+					'Content-type': 'application/json'
+				}
+			}).then(function (data) {
+			return data.json();
+		})
+	};
 
-    var getByKey = function (id) {
-        return window.fetch(baseUrl + 'chats/thread/' + id + '/',
-            {
-                method: 'GET',
-                credentials: 'include',
-                headers: {
-                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
-                    Accept: 'application/json',
-                    'Content-type': 'application/json'
-                }
-            }).then(function (data) {
-            return data.json();
-        })
-    };
+	var getByKey = function (id) {
+		return window.fetch(baseUrl + 'chats/thread/' + id + '/',
+			{
+				method: 'GET',
+				credentials: 'include',
+				headers: {
+					'X-CSRFToken': cookieService.getCookie('csrftoken'),
+					Accept: 'application/json',
+					'Content-type': 'application/json'
+				}
+			}).then(function (data) {
+			return data.json();
+		})
+	};
 
-    var create = function (thread) {
-        return window.fetch(baseUrl + 'chats/thread/',
-            {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
-                    Accept: 'application/json',
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(thread)
-            }).then(function (data) {
-            return data.json();
-        })
-    };
+	var create = function (thread) {
+		return window.fetch(baseUrl + 'chats/thread/',
+			{
+				method: 'POST',
+				credentials: 'include',
+				headers: {
+					'X-CSRFToken': cookieService.getCookie('csrftoken'),
+					Accept: 'application/json',
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(thread)
+			}).then(function (data) {
+			return data.json();
+		})
+	};
 
-    var update = function (id, thread) {
-        return window.fetch(baseUrl + 'chats/thread/' + id + '/',
-            {
-                method: 'PUT',
-                credentials: 'include',
-                headers: {
-                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
-                    Accept: 'application/json',
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(thread)
-            }).then(function (data) {
-            return data.json();
-        })
-    };
+	var update = function (id, thread) {
+		return window.fetch(baseUrl + 'chats/thread/' + id + '/',
+			{
+				method: 'PUT',
+				credentials: 'include',
+				headers: {
+					'X-CSRFToken': cookieService.getCookie('csrftoken'),
+					Accept: 'application/json',
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(thread)
+			}).then(function (data) {
+			return data.json();
+		})
+	};
 
-    var deleteByKey = function (id) {
-        return window.fetch(baseUrl + 'chats/thread/' + id + '/',
-            {
-                method: 'DELETE',
-                credentials: 'include',
-                headers: {
-                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
-                    Accept: 'application/json',
-                    'Content-type': 'application/json'
-                }
-            }).then(function (data) {
-            return new Promise(function(resolve,reject) {
-                resolve({status: 'deleted'});
-            });
-            //return data.json();
-        })
-    };
+	var deleteByKey = function (id) {
+		return window.fetch(baseUrl + 'chats/thread/' + id + '/',
+			{
+				method: 'DELETE',
+				credentials: 'include',
+				headers: {
+					'X-CSRFToken': cookieService.getCookie('csrftoken'),
+					Accept: 'application/json',
+					'Content-type': 'application/json'
+				}
+			}).then(function (data) {
+			return new Promise(function(resolve,reject) {
+				resolve({status: 'deleted'});
+			});
+			//return data.json();
+		})
+	};
 
-    module.exports = {
-        getList: getList,
-        getByKey: getByKey,
-        create: create,
-        update: update,
-        deleteByKey: deleteByKey
-    }
+	module.exports = {
+		getList: getList,
+		getByKey: getByKey,
+		create: create,
+		update: update,
+		deleteByKey: deleteByKey
+	}
 
 }());
