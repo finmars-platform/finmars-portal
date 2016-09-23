4c19bee694f937255ceb90cf2d5686411f71b503
Сергей Житенев < >
2016-09-23 14:11:13 +0300

    fix mapping config

--- src/portal/scripts/app/directives/groupTable/gClipboardHandlerComponent.js
+++ src/portal/scripts/app/directives/groupTable/gClipboardHandlerComponent.js
@@ -19,62 +19,73 @@
 
                 var handler = function (e) {
 
-                    var copiedItems = [];
+                    if (e) {
 
-                    scope.items.forEach(function (item) {
-                        //console.log('item', item);
-                        if (item.hasOwnProperty('groups')) {
-                            if (item.selectedRow === true) {
-                                copiedItems.push({type: 'group', data: item.groups});
-                            }
-                            item.items.forEach(function (row) {
-                                //console.log('row', row);
-                                if (row.selectedRow === true) {
-                                    copiedItems.push(row);
+                        var copiedItems = [];
+
+                        scope.items.forEach(function (item) {
+                            //console.log('item', item);
+                            if (item.hasOwnProperty('groups')) {
+                                if (item.selectedRow === true) {
+                                    copiedItems.push({type: 'group', data: item.groups});
                                 }
-                            })
-                        } else {
-                            if (item.selectedRow === true) {
-                                copiedItems.push(item);
-                            }
-                        }
-                    });
-
-                    //console.log(copiedItems);
-
-                    var result = '<table>';
-                    copiedItems.forEach(function (item) {
-                        var row = '<tr>';
-
-                        if (item.hasOwnProperty('type')) {
-                            row = row + '<td>' + item.data.map(function (item) {
-                                    return item.value
-                                }).join(' ') + '</td>';
-                        } else {
-                            scope.columns.forEach(function (column) {
-                                if (column.hasOwnProperty('key')) {
-                                    row = row + '<td>' + item[column.key] + '</td>';
-                                } else {
-                                    row = row + '<td>' + item[column.name] + '</td>';
+                                item.items.forEach(function (row) {
+                                    //console.log('row', row);
+                                    if (row.selectedRow === true) {
+                                        copiedItems.push(row);
+                                    }
+                                })
+                            } else {
+                                if (item.selectedRow === true) {
+                                    copiedItems.push(item);
                                 }
-                            });
+                            }
+                        });
+
+                        //console.log(copiedItems);
+
+                        var result = '<table>';
+                        copiedItems.forEach(function (item) {
+                            var row = '<tr>';
+
+                            if (item.hasOwnProperty('type')) {
+                                row = row + '<td>' + item.data.map(function (item) {
+                                        return item.value
+                                    }).join(' ') + '</td>';
+                            } else {
+                                scope.columns.forEach(function (column) {
+                                    if (column.hasOwnProperty('key')) {
+                                        row = row + '<td>' + item[column.key] + '</td>';
+                                    } else {
+                                        row = row + '<td>' + item[column.name] + '</td>';
+                                    }
+                                });
+                            }
+
+
+                            row = row + '</tr>';
+                            result = result + row;
+                        });
+                        result = result + '</table';
+
+                        console.log('result', result);
+                        if (e.clipboardData) {
+                            e.clipboardData.setData('text/html', result);
                         }
-
-
-                        row = row + '</tr>';
-                        result = result + row;
-                    });
-                    result = result + '</table';
-
-                    console.log('result', result);
-                    e.clipboardData.setData('text/html', result);
-                    console.log('e', e);
-                    e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
+                        console.log('e', e);
+                        e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
+                    }
                 };
 
                 document.addEventListener('copy', handler);
 
                 $(document).bind('copy', handler);
+
+                scope.$on("destroy", function () {
+                    logService.component('groupClipboardHandlerComponent', 'destroyed');
+                    $(document).unbind('copy');
+                    document.removeEventListener('copy');
+                })
             }
         }
 
