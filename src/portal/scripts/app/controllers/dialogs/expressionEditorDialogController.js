2291cfda401a15f008554e2428e2c293661e4e72
Sergei Zhitenev <s.zhitenev@finmars.com>
2017-01-13 17:01:40 +0300

    expression builder help

--- src/portal/scripts/app/controllers/dialogs/expressionEditorDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/expressionEditorDialogController.js
@@ -7,13 +7,50 @@
 
     var logService = require('../../../../../core/services/logService');
 
+    var helpService = require('../../services/helpService');
+
     module.exports = function ($scope, $mdDialog, item) {
 
         logService.controller('ExpressionEditorDialogController', 'initialized');
 
         var vm = this;
 
+        vm.readyStatus = {expression: false};
+
         vm.item = item;
+
+        helpService.getFunctionsHelp().then(function (data) {
+            vm.expressions = data;
+
+            vm.readyStatus.expression = true;
+
+            vm.selectedHelpItem = vm.expressions[0];
+            $scope.$apply();
+        });
+
+        vm.selectHelpItem = function (item) {
+            vm.expressions.forEach(function (expr) {
+                expr.isSelected = false;
+            });
+
+            item.isSelected = true;
+
+            vm.selectedHelpItem = item;
+        };
+
+        vm.appendFunction = function (item) {
+
+            console.log(this);
+            var val = $('#editorExpressionInput')[0].value;
+            var cursorPosition = val.slice(0, $('#editorExpressionInput')[0].selectionStart).length;
+
+            if (cursorPosition == 0) {
+                vm.item.expression = vm.item.expression + item.func;
+            } else {
+                vm.item.expression = vm.item.expression.slice(0, cursorPosition) + item.func + vm.item.expression.slice(cursorPosition);
+
+            }
+        };
 
         vm.cancel = function () {
             $mdDialog.cancel();
