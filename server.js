a7ee128e9acd96b8f255e983fc1d8c2e1764db61
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-04-24 20:50:03 +0300

    update

--- server.js
+++ server.js
@@ -3,22 +3,18 @@
  */
 'use strict';
 var express = require('express');
-  var url = require('url');
+var url = require('url');
 var proxy = require('proxy-middleware');
 var app = express();
 
 app.use(express.static('dist'));
+app.use('/portal', express.static('dist'));
 
-var proxyOptions = url.parse('http://ec2-18-196-215-43.eu-central-1.compute.amazonaws.com/');
-proxyOptions.cookieRewrite = true;
+// var proxyOptions = url.parse('http://127.0.0.1:8000');
+// proxyOptions.cookieRewrite = true;
+//
+// app.use('/', proxy(proxyOptions));
 
-app.use('/', proxy(proxyOptions));
-
-// app.use(function(req, res){
-//   console.info(req);
-//   console.info('123');
-// });
-
-app.listen(3000, '0.0.0.0', function(){
-  console.info('Express server start at 3000 port');
+app.listen(8080, '0.0.0.0', function(){
+  console.info('Express server start at 8080 port');
 });
--- src/portal/scripts/main.js
+++ src/portal/scripts/main.js
@@ -89,6 +89,7 @@ app.controller('TransactionMappingEditDialogController', ['$scope', '$mdDialog',
 app.controller('TransactionMappingInputMappingDialogController', ['$scope', '$mdDialog', 'data', require('./app/controllers/dialogs/transactionMappingInputMappingDialogController')]);
 
 app.controller('EntityMappingEditDialogController', ['$scope', '$mdDialog', 'schemeId', require('./app/controllers/dialogs/entityMappingEditDialogController')]);
+app.controller('EntityMappingCreateDialogController', ['$scope', '$mdDialog', require('./app/controllers/dialogs/entityMappingCreateDialogController')]);
 
 app.controller('EntityViewerController', ['$scope', '$mdDialog', require('./app/controllers/entityViewer/entityViewerController')]);
 app.controller('EntityViewerAddDialogController', ['$scope', '$mdDialog', 'parentScope', '$state', require('./app/controllers/entityViewer/entityViewerAddDialogController')]);
