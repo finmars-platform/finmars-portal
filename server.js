6ebdd0c66ca2ac74a2b89b36f1ba16f396f5cdb0
szhitenev <szhitenev>
2018-04-26 22:08:34 +0300

    update server.js & remove bower dependency

--- server.js
+++ server.js
@@ -3,17 +3,22 @@
  */
 'use strict';
 var express = require('express');
+var path = require('path');
 var url = require('url');
 var proxy = require('proxy-middleware');
 var app = express();
 
-app.use(express.static('dist'));
-app.use('/portal', express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
+// app.use('/portal', express.static('dist'));
 
 // var proxyOptions = url.parse('http://127.0.0.1:8000');
 // proxyOptions.cookieRewrite = true;
 //
 // app.use('/', proxy(proxyOptions));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/' + "index.html"));
+});
 
 app.listen(8080, '0.0.0.0', function(){
   console.info('Express server start at 8080 port');
