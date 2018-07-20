8b1f7122ea541e196e22be7d46e4246fddf186f6
szhitenev <szhitenev>
2018-07-20 13:47:42 +0300

    fix entityUrlService

--- src/portal/scripts/app/services/entityUrlService.js
+++ src/portal/scripts/app/services/entityUrlService.js
@@ -7,29 +7,64 @@
 
     var resolve = function (entityType) {
 
-        switch (entityType) {
-
-            case 'portfolio':
-                return 'portfolios/portfolio';
-            case 'account':
-                return 'accounts/account';
-            case 'responsible':
-                return 'counterparties/responsible';
-            case 'counterparty':
-                return 'counterparties/counterparty';
-            case 'instrument':
-                return 'instruments/instrument';
-            case 'transaction':
-                return 'transactions/transaction';
-            case 'currency':
-                return 'currencies/currency'
-
-
-        }
     };
-
     module.exports = {
         resolve: resolve
+    }
+
+    switch (entityType) {
+
+        case 'portfolio':
+            return 'portfolios/portfolio';
+        case 'account':
+            return 'accounts/account';
+        case 'account-type':
+            return 'accounts/account-type';
+        case 'responsible':
+            return 'counterparties/responsible';
+        case 'responsible-group':
+            return 'counterparties/responsible-group';
+        case 'counterparty':
+            return 'counterparties/counterparty';
+        case 'counterparty-group':
+            return 'counterparties/counterparty-group';
+        case 'instrument':
+            return 'instruments/instrument';
+        case 'instrument-type':
+            return 'instruments/instrument-type';
+        case 'price-history':
+            return 'instruments/price-history';
+        case 'transaction':
+            return 'transactions/transaction';
+        case 'transaction-type':
+            return 'transactions/transaction-type';
+        case 'complex-transaction':
+            return 'transaction/complex-transaction';
+        case 'currency':
+            return 'currencies/currency';
+        case 'currency-history':
+            return 'currencies/currency-history';
+        case 'pricing-policy':
+            return 'instruments/pricing-policy';
+        case 'strategy-1':
+            return 'strategies/1/strategy';
+        case 'strategy-1-subgroup':
+            return 'strategies/1/subgroup';
+        case 'strategy-1-group':
+            return 'strategies/1/group';
+        case 'strategy-2':
+            return 'strategies/2/strategy';
+        case 'strategy-2-subgroup':
+            return 'strategies/2/subgroup';
+        case 'strategy-2-group':
+            return 'strategies/2/group';
+        case 'strategy-3':
+            return 'strategies/3/strategy';
+        case 'strategy-3-subgroup':
+            return 'strategies/3/subgroup';
+        case 'strategy-3-group':
+            return 'strategies/3/group'
+
     }
 
 }());
