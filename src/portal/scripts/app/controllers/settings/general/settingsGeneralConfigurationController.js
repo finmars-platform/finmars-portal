b71bd032e7f0d4059f630ca7b3ad9552854830b8
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-08-21 15:38:21 +0300

    configuration import / export

---
+++ src/portal/scripts/app/controllers/settings/general/settingsGeneralConfigurationController.js
@@ -0,0 +1,76 @@
+/**
+ * Created by szhitenev on 17.08.2016.
+ */
+(function () {
+
+    var configurationService = require('../../../services/configurationService');
+
+    module.exports = function ($scope, $mdDialog) {
+
+        var vm = this;
+
+        vm.configurationFile = null;
+
+        vm.loadIsAvailable = function () {
+            return vm.configurationFile !== null && vm.configurationFile !== undefined
+        };
+
+        vm.importConfiguration = function ($event) {
+
+            var reader = new FileReader();
+
+            reader.readAsText(vm.configurationFile);
+
+            reader.onload = function (evt) {
+
+                try {
+
+                    var file = JSON.parse(evt.target.result);
+
+                    $mdDialog.show({
+                        controller: 'SettingGeneralConfigurationPreviewFileDialogController as vm',
+                        templateUrl: 'views/dialogs/settings-general-configuration-preview-file-dialog-view.html',
+                        parent: angular.element(document.body),
+                        targetEvent: $event,
+                        preserveScope: true,
+                        autoWrap: true,
+                        skipHide: true,
+                        locals: {
+                            file: file
+                        }
+                    }).then(function (res) {
+
+                        vm.configurationFile = null;
+
+                    });
+
+                } catch (error) {
+
+                    $mdDialog.show({
+                        controller: 'WarningDialogController as vm',
+                        templateUrl: 'views/warning-dialog-view.html',
+                        parent: angular.element(document.body),
+                        targetEvent: $event,
+                        clickOutsideToClose: true,
+                        locals: {
+                            warning: {
+                                title: 'Error',
+                                description: 'Unable to read it. This file is corrupted.'
+                            }
+                        },
+                        preserveScope: true,
+                        autoWrap: true,
+                        skipHide: true
+                    });
+
+                    vm.configurationFile = null;
+
+                }
+
+            }
+
+        }
+
+    }
+
+}());
