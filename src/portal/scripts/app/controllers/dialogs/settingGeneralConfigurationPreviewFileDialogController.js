9285ce828e4f11cf9bf344a06a31d91610a4577e
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-08-23 13:44:48 +0300

    add transaction type group dependency handle

--- src/portal/scripts/app/controllers/dialogs/settingGeneralConfigurationPreviewFileDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/settingGeneralConfigurationPreviewFileDialogController.js
@@ -50,6 +50,10 @@
                 return item.name
             }
 
+            if (item.hasOwnProperty('content_type')) {
+                return item.content_type
+            }
+
         };
 
         vm.toggleActiveForChilds = function (item) {
@@ -76,6 +80,110 @@
 
 
         };
+
+        function isEntitySelected(entity) {
+
+            var result = false;
+
+            entity.content.forEach(function (item) {
+
+                if (item.active) {
+                    result = true;
+                }
+
+            });
+
+            return result;
+
+        }
+
+        function findEntity(items, entityName) {
+
+            var result;
+
+            items.forEach(function (item) {
+
+                if (item.entity === entityName) {
+                    result = item;
+                }
+
+            });
+
+            return result;
+
+        }
+
+        function handleTransactionTypeGroupDependency(transactionTypeEntity, depGroups) {
+
+            return new Promise(function (resolve, reject) {
+
+                entityResolverService.getList('transaction-type-group').then(function (data) {
+
+                    var groups = data.results;
+
+                    console.log('transactionTypeEntity', transactionTypeEntity);
+                    console.log('depGroups', depGroups);
+
+                    var groupsExists = [];
+
+                    depGroups.forEach(function (depGroup) {
+
+                        groups.forEach(function (group) {
+
+                            if (group.user_code === depGroup.user_code) {
+                                groupsExists.push(depGroup.user_code)
+                            }
+
+                        })
+
+                    });
+
+                    var promises = [];
+
+                    depGroups.forEach(function (depGroup) {
+
+                        if (groupsExists.indexOf(depGroup.user_code) === -1) {
+                            promises.push(entityResolverService.create('transaction-type-group', depGroup))
+                        }
+
+                    });
+
+                    Promise.all(promises).then(function (data) {
+
+                        entityResolverService.getList('transaction-type-group').then(function (data) {
+
+                            var groups = data.results;
+
+                            transactionTypeEntity.content.forEach(function (item) {
+
+                                groups.forEach(function (group) {
+
+                                    if (item.hasOwnProperty('___group_user_code')) {
+
+                                        if (item.___group_user_code === group.user_code) {
+                                            item.group = group.id;
+                                        }
+
+                                    }
+
+                                })
+
+                            });
+
+                            console.log(transactionTypeEntity);
+
+                            resolve(transactionTypeEntity);
+
+                        });
+
+                    });
+
+
+                });
+
+            })
+
+        }
 
         function handleTransactionType(item) {
             return new Promise(function (resolve, reject) {
@@ -114,50 +222,98 @@
             })
         }
 
-        vm.agree = function () {
+        function importConfiguration(items) {
 
-            var promises = [];
+            return new Promise(function (resolve, reject) {
 
-            vm.items.forEach(function (entityItem) {
+                var promises = [];
 
-                entityItem.content.forEach(function (item) {
+                items.forEach(function (entityItem) {
 
-                    if (item.active) {
+                    entityItem.content.forEach(function (item) {
 
-                        switch (entityItem.entity) {
+                        if (item.active) {
 
-                            case 'transactions.transactiontype':
-                                promises.push(handleTransactionType(item));
-                                break;
-                            case 'ui.editlayout':
-                                promises.push(handleEditLayout(item));
-                                break;
-                            case 'ui.listlayout':
-                                promises.push(handleListLayout(item));
-                                break;
-                            case 'csv_import.scheme':
-                                promises.push(handleCsvImportScheme(item));
-                                break;
-                            case 'integrations.instrumentdownloadscheme':
-                                promises.push(handleInstrumentDownloadScheme(item));
-                                break;
-                            case 'integrations.pricedownloadscheme':
-                                promises.push(handlePriceDownloadScheme(item));
-                                break;
+                            switch (entityItem.entity) {
+
+                                case 'transactions.transactiontype':
+                                    promises.push(handleTransactionType(item));
+                                    break;
+                                case 'ui.editlayout':
+                                    promises.push(handleEditLayout(item));
+                                    break;
+                                case 'ui.listlayout':
+                                    promises.push(handleListLayout(item));
+                                    break;
+                                case 'csv_import.scheme':
+                                    promises.push(handleCsvImportScheme(item));
+                                    break;
+                                case 'integrations.instrumentdownloadscheme':
+                                    promises.push(handleInstrumentDownloadScheme(item));
+                                    break;
+                                case 'integrations.pricedownloadscheme':
+                                    promises.push(handlePriceDownloadScheme(item));
+                                    break;
+
+                            }
 
                         }
 
-                    }
+                    })
+
+                });
+
+                Promise.all(promises).then(function (data) {
+
+                    console.log("import success", data);
+
+                    resolve(data);
+
 
                 })
 
-            });
+            })
 
-            Promise.all(promises).then(function (data) {
+        }
 
-                console.log("import success", data);
+        function initPreparations(items) {
 
-                $mdDialog.hide({status: 'agree', data: {}});
+            return new Promise(function (resolve, reject) {
+
+                var transactionTypeEntity = findEntity(items, 'transactions.transactiontype');
+
+                var promises = [];
+
+                if (isEntitySelected(transactionTypeEntity) && transactionTypeEntity.dependencies.length) {
+
+                    transactionTypeEntity.dependencies.forEach(function (dependency) {
+
+                        switch (dependency.entity) {
+
+                            case 'transactions.transactiontypegroup':
+                                promises.push(handleTransactionTypeGroupDependency(transactionTypeEntity, dependency.content));
+                                break;
+                        }
+
+                    });
+
+                }
+
+                Promise.all(promises).then(function (data) {
+                    resolve(data);
+                })
+
+            })
+
+        }
+
+        vm.agree = function () {
+
+            initPreparations(vm.items).then(function (value) {
+
+                // importConfiguration(vm.items).then(function (value) {
+                //     $mdDialog.hide({status: 'agree', data: {}});
+                // })
 
             })
 
