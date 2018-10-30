702a44565d619169f61f85a784b2f931f2e2a192
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-10-30 13:05:22 +0300

    sync

---
+++ src/portal/scripts/app/services/expression.service.js
@@ -0,0 +1,42 @@
+(function () {
+
+    'use strict';
+
+    var baseUrlService = require('../services/baseUrlService');
+    var cookieService = require('../../../../core/services/cookieService');
+    var baseUrl = baseUrlService.resolve();
+
+    var validate = function (data) {
+
+        if (!data.hasOwnProperty('is_eval')) {
+            data.is_eval = false;
+        }
+
+        return window.fetch(baseUrl + 'utils/expression/',
+            {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
+                    Accept: 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(function (response) {
+
+                if (!response.ok) {
+                    throw response;
+                }
+
+                return response.json()
+            })
+
+
+    };
+
+    module.exports = {
+        validate: validate
+    }
+
+}());
