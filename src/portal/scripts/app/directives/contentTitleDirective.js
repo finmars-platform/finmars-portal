fda84e07e010d4bcb418d26b6da82eff0e4e4e1a
Sergei Zhitenev <s.zhitenev@finmars.com>
2018-11-09 16:49:33 +0300

    add titles to labels in transaction action

---
+++ src/portal/scripts/app/directives/contentTitleDirective.js
@@ -0,0 +1,14 @@
+(function () {
+
+    'use strict';
+
+    module.exports = function ($timeout) {
+        return {
+            link: function(scope, element) {
+                $timeout(function() {
+                    element[0].title = element[0].textContent;
+                });
+            }
+        };
+    }
+}());
