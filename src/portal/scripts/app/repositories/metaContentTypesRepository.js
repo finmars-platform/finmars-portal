c96dda09cc80024c02799bbe09ff7594ba9a3bd6
szhitenev <szhitenev>
2018-12-12 14:10:04 +0300

    transaction type import

--- src/portal/scripts/app/repositories/metaContentTypesRepository.js
+++ src/portal/scripts/app/repositories/metaContentTypesRepository.js
@@ -105,6 +105,176 @@
         ]
     };
 
+    var getList = function () {
+        return [
+            {
+                name: "Dashboard",
+                entity: 'dashboard',
+                key: "ui.dashboard"
+            },
+            {
+                name: "Account Type",
+                entity: 'account-type',
+                key: "accounts.accounttype"
+            },
+            {
+                name: "Account",
+                entity: 'account',
+                key: "accounts.account"
+            },
+            {
+                name: "Counterparty",
+                entity: 'counterparty',
+                key: "counterparties.counterparty"
+            },
+            {
+                name: "Responsible",
+                entity: 'responsible',
+                key: "counterparties.responsible"
+            },
+            {
+                name: "Currency",
+                entity: 'currency',
+                key: "currencies.currency"
+            },
+            {
+                name: "Currency history",
+                entity: 'currency-history',
+                key: "currencies.currencyhistory"
+            },
+            {
+                name: "Instrument",
+                entity: 'instrument',
+                key: "instruments.instrument"
+            },
+            {
+                name: 'Pricing Policy',
+                entity: 'pricing-policy',
+                key: 'instruments.pricingpolicy'
+            },
+            {
+                name: 'Price History',
+                entity: 'price-history',
+                key: 'instruments.pricehistory'
+            },
+            {
+                name: "Portfolio",
+                entity: 'portfolio',
+                key: "portfolios.portfolio"
+            },
+            {
+                name: "Instrument Type",
+                entity: 'instrument-type',
+                key: "instruments.instrumenttype"
+            },
+            {
+                name: "Transaction",
+                entity: 'transaction',
+                key: "transactions.transaction"
+            },
+            {
+                name: "Transaction Type",
+                entity: 'transaction-type',
+                key: "transactions.transactiontype"
+            },
+            {
+                name: "Transaction Type Group",
+                entity: 'transaction-type-group',
+                key: "transactions.transactiontypegroup"
+            },
+            {
+                name: "Counterparty group",
+                entity: 'counterparty-group',
+                key: "counterparties.counterpartygroup"
+            },
+            {
+                name: "Responsible group",
+                entity: 'responsible-group',
+                key: "counterparties.responsiblegroup"
+            },
+            {
+                name: "Strategy 1",
+                entity: 'strategy-1',
+                key: "strategies.strategy1"
+            },
+            {
+                name: "Strategy 2",
+                entity: 'strategy-2',
+                key: "strategies.strategy2"
+            },
+            {
+                name: "Strategy 3",
+                entity: 'strategy-3',
+                key: "strategies.strategy3"
+            },
+            {
+                name: "Strategy 1 group",
+                entity: 'strategy-1-group',
+                key: "strategies.strategy1group"
+            },
+            {
+                name: "Strategy 2 group",
+                entity: 'strategy-2-group',
+                key: "strategies.strategy2group"
+            },
+            {
+                name: "Strategy 3 group",
+                entity: 'strategy-3-group',
+                key: "strategies.strategy3group"
+            },
+            {
+                name: "Strategy 1 subgroup",
+                entity: 'strategy-1-subgroup',
+                key: "strategies.strategy1subgroup"
+            },
+            {
+                name: "Strategy 2 subgroup",
+                entity: 'strategy-2-subgroup',
+                key: "strategies.strategy1subgroup"
+            },
+            {
+                name: "Strategy 3 subgroup",
+                entity: 'strategy-3-subgroup',
+                key: "strategies.strategy1subgroup"
+            },
+            {
+                name: "Tag",
+                entity: 'tag',
+                key: "tags.tag"
+            },
+            {
+                name: "Balance report",
+                entity: 'balance-report',
+                key: "reports.balancereport"
+            },
+            {
+                name: "P&L report",
+                entity: 'pnl-report',
+                key: "reports.plreport"
+            },
+            {
+                name: "Transaction report",
+                entity: 'transaction-report',
+                key: "reports.transactionreport"
+            },
+            {
+                name: "Cash flow projection report",
+                entity: 'cash-flow-projection-report',
+                key: "reports.cashflowreport"
+            },
+            {
+                name: "Performance report",
+                entity: 'performance-report',
+                key: "reports.performancereport"
+            },
+            {
+                name: "Transaction",
+                entity: 'complex-transaction',
+                key: "transactions.complextransaction"
+            }
+        ]
+    };
+
     var getListForTags = function () {
 
         return [
@@ -474,6 +644,7 @@
     module.exports = {
         getListForTags: getListForTags,
         getListForUi: getListForUi,
+        getList: getList,
         getListForTransactionTypeInputs: getListForTransactionTypeInputs,
         getContentTypeList: getContentTypeList,
         getListForDataImport: getListForDataImport
