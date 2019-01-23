c5afab0c117a746caf6c984a28527a7262d83843
Marscap <marscap@Sergejs-MacBook-Pro.local>
2019-01-23 22:22:20 +0100

    backward compatibility support in import configuration

--- src/portal/scripts/app/models/accountTypePropsModel.js
+++ src/portal/scripts/app/models/accountTypePropsModel.js
@@ -10,42 +10,51 @@
             {
                 "key": "name",
                 "name": "Name",
-                "value_type": 10
+                "value_type": 10,
+                "allow_null": false
+
             },
             {
                 "key": "short_name",
                 "name": "Short name",
-                "value_type": 10
+                "value_type": 10,
+                "allow_null": true
             },
             {
                 "key": "notes",
                 "name": "Notes",
-                "value_type": 10
+                "value_type": 10,
+                "allow_null": true
             },
             {
                 "key": "public_name",
                 "name": "Public name",
-                "value_type": 10
+                "value_type": 10,
+                "allow_null": true
             },
             {
                 "key": "user_code",
                 "name": "User code",
-                "value_type": 10
+                "value_type": 10,
+                "allow_null": true
             },
             {
                 "key": "show_transaction_details",
                 "name": "Show transaction details",
-                "value_type": "boolean"
+                "value_type": "boolean",
+                "allow_null": true
             },
             {
                 "key": "transaction_details_expr",
                 "name": "Transaction details expr",
-                "value_type": 10
+                "value_type": 10,
+                "allow_null": true
             },
             {
                 "key": "tags",
                 "name": "Tags",
-                "value_type": "mc_field"
+                "value_type": "mc_field",
+                "allow_null": true
             }
         ]
     };
