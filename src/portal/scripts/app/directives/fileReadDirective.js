fa5d7ceebdc74c31932efaf5440b09d10ee6d2f1
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-02-15 17:55:11 +0300

    transaction import file validation

--- src/portal/scripts/app/directives/fileReadDirective.js
+++ src/portal/scripts/app/directives/fileReadDirective.js
@@ -9,28 +9,20 @@
         return {
             restrict: 'AE',
             scope: {
-                'fileRead': '='
+                'fileRead': '=',
+                'fileReadChange': '&'
             },
             link: function (scope, elem, attrs) {
 
-                console.log('scope?13124',scope);
+                console.log('scope?13124', scope);
 
                 elem.bind('change', function (changeEvent) {
-                    //
-                    //console.log('change', changeEvent);
-                    //var reader = new FileReader();
-                    //
-                    //reader.onload = function (loadEvent) {
-                    //
-                    //    scope.$apply(function () {
-                    //        scope.fileRead = loadEvent.target.result;
-                    //    })
-                    //
-                    //};
-                    //
-                    //reader.readAsDataURL(changeEvent.target.files[0]);
 
                     scope.fileRead = changeEvent.target.files[0];
+
+                    if (scope.fileReadChange) {
+                        scope.fileReadChange()
+                    }
 
                 })
             }
