5c8973c8dd9c25d8323d0b05b14fe26fe32d2cbf
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-02-18 20:40:09 +0300

    events fix?

--- src/portal/scripts/app/controllers/dialogs/importTransactionDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/importTransactionDialogController.js
@@ -193,7 +193,7 @@
 
                 if (vm.config.task_status === 'SUCCESS') {
 
-                    if (vm.config.error_rows.length === 0) {
+                    if (vm.config.error_rows.length === 0 && !vm.config.error_message) {
 
                         vm.finishedSuccess = true;
 
