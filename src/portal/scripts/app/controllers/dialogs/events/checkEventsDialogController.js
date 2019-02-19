7f834b6628559aa7390bfb152ec6b9009bb44c60
szhitenev <szhitenev>
2019-02-19 22:39:48 +0300

    loading state

--- src/portal/scripts/app/controllers/dialogs/events/checkEventsDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/events/checkEventsDialogController.js
@@ -291,7 +291,8 @@
                 case 3:
                     return 'Booked (system, default)';
                 case 4:
-                    return 'Booked (user, actions)';
+                    // return 'Booked (user, actions)';
+                    return 'Booked (user)';
                 // case 5:
                 //     return 'Booked, pending (system)';
                 case 6:
