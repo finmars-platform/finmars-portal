e96b4d795587f5e5030137187387a7ea9e87bf60
mevstratov <m.evstratov@finmars.com>
2019-02-27 23:17:07 +0300

    notifications settings button in actions, smart search table header development

---
+++ src/portal/scripts/app/controllers/dialogs/actionsNotificationsSettingsDialogController.js
@@ -0,0 +1,46 @@
+/**
+ * Created by mevstratov on 27.02.2019.
+ */
+(function () {
+
+    'use strict';
+
+    var logService = require('../../../../../core/services/logService');
+
+    var timeZonesService = require('../../services/timeZonesService');
+
+    var usersService = require('../../services/usersService');
+
+    module.exports = function ($scope, $mdDialog) {
+        logService.controller('ActionsNotificationsSettingsDialogController', 'initialized');
+
+        var vm = this;
+
+        vm.readyStatus = {member: false};
+
+        vm.timeZones = timeZonesService.getList();
+
+        usersService.getOwnMemberSettings().then(function (data) {
+            vm.member = data.results[0];
+            vm.readyStatus.member = true;
+            $scope.$apply();
+        });
+
+        vm.cancel = function () {
+          $mdDialog.cancel();
+        };
+
+        vm.save = function () {
+            usersService.updateOwnMemberSettings(vm.member.id, vm.member).then(function (response) {
+
+                $mdDialog.hide({status: 'success'});
+
+            }, function (error) {
+
+                console.log('notifications error', error);
+                $mdDialog.hide({status: error});
+
+            });
+        };
+    }
+}());
