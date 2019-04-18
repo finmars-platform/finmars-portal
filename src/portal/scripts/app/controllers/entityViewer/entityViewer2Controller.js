3f58f0f5bd069867b0d23ffb0036a28d8c920ca2
mevstratov <m.evstratov@finmars.com>
2019-04-18 22:48:35 +0300

    layout change warning

--- src/portal/scripts/app/controllers/entityViewer/entityViewer2Controller.js
+++ src/portal/scripts/app/controllers/entityViewer/entityViewer2Controller.js
@@ -13,11 +13,13 @@
         var EntityViewerDataService = require('../../services/entityViewerDataService');
         var EntityViewerEventService = require('../../services/entityViewerEventService');
 
+        var stringHelper = require('../../helpers/stringHelper');
+
         var evDataProviderService = require('../../services/ev-data-provider/ev-data-provider.service');
 
         var entityViewerReducer = require('./entityViewerReducer');
 
-        module.exports = function ($scope, $mdDialog) {
+        module.exports = function ($scope, $mdDialog, $transitions) {
 
             var vm = this;
 
@@ -101,10 +103,94 @@
 
                 entityViewerReducer.initReducer(entityViewerDataService, entityViewerEventService, $mdDialog, vm.getView);
 
-
             };
 
             vm.init();
+
+            var checkLayoutForChanges = function (transition) {
+                var stateName = transition.to().name;
+                var listOfStatesWithLayout = [
+                    'app.data.portfolio',
+                    'app.data.account',
+                    'app.data.account-type',
+                    'app.data.counterparty',
+                    'app.data.responsible',
+                    'app.data.instrument',
+                    'app.data.instrument-type',
+                    'app.data.pricing-policy',
+                    'app.data.complex-transaction',
+                    'app.data.transaction',
+                    'app.data.transaction-type',
+                    'app.data.currency-history',
+                    'app.data.price-history',
+                    'app.data.currency',
+                    'app.data.strategy-group',
+                    'app.data.strategy'
+                ];
+
+                console.log("layout warning transition data", transition, stateName);
+                if (listOfStatesWithLayout.indexOf(stateName) !== -1) {
+
+                    var th = $('.g-columns-component.g-thead').find('.g-cell');
+                    var thWidths = [];
+                    for (var i = 0; i < th.length; i++) {
+                        var thWidth = $(th[i]).width();
+                        thWidths.push(thWidth);
+                    }
+
+                    var activeLayout = JSON.stringify(entityViewerDataService.getListLayout());
+                    var layoutCurrentConfiguration = JSON.stringify(entityViewerDataService.getLayoutCurrentConfiguration(thWidths, false));
+                    console.log("layout warning layouts", JSON.stringify(activeLayout) + '\n' + JSON.stringify(layoutCurrentConfiguration));
+
+                    var activeLayoutHash = stringHelper.toHash(activeLayout);
+                    var layoutCurrentConfigurationHash = stringHelper.toHash(layoutCurrentConfiguration);
+
+                    console.log("layout warning hashes", activeLayoutHash + '\n' + layoutCurrentConfigurationHash);
+
+                    if (activeLayoutHash !== layoutCurrentConfigurationHash) {
+
+                        return new Promise (function (resolve, reject) {
+
+                            $mdDialog.show({
+                                controller: 'WarningDialogController as vm',
+                                templateUrl: 'views/warning-dialog-view.html',
+                                parent: angular.element(document.body),
+                                preserveScope: true,
+                                autoWrap: true,
+                                multiple: true,
+                                skipHide: true,
+                                locals: {
+                                    warning: {
+                                        title: 'Warning',
+                                        description: 'If you leave this page all unsaved layout changes will be lost. Do you want to proceed.'
+                                    }
+                                }
+                            }).then(function (res, rej) {
+
+                                if (res.status === 'agree') {
+                                    resolve(true);
+                                }
+                            }).catch(function () {
+                                reject();
+                            });
+                        });
+
+                    } else {
+                        console.log('layout warning layout is not modified');
+                    }
+                } else {
+                    console.log('layout warning without layout');
+                }
+
+            };
+
+            // var doBeforeStateChange = $transitions.onBefore({}, checkLayoutForChanges);
+
+
+            this.$onDestroy = function () {
+                // doBeforeStateChange();
+                console.log("layout warning destroyed");
+            }
         }
 
     }()
