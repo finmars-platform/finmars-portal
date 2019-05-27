8fd8d66e1e38298774dff70a2f398f25c68f6e90
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-05-27 15:24:48 +0300

    user-filtser service

---
+++ src/portal/scripts/app/services/rv-data-provider/user-filter.service.js
@@ -0,0 +1,30 @@
+(function () {
+
+    var getDataByKey = function (entityViewerDataService, key) {
+
+        var data = entityViewerDataService.getFlatList();
+        var result = [];
+
+        data.forEach(function (item) {
+
+            if (item.hasOwnProperty(key)) {
+
+                if (result.indexOf(item[key]) === -1) {
+
+                    result.push(item[key]);
+
+                }
+
+            }
+
+        });
+
+        return result;
+
+    };
+
+    module.exports = {
+        getDataByKey: getDataByKey
+    }
+
+}());
