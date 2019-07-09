de52c7c0f5284a323ecd780e2541a07ba70ecf02
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-07-09 19:09:30 +0300

    sync

--- src/portal/scripts/app/controllers/pages/complexImportController.js
+++ src/portal/scripts/app/controllers/pages/complexImportController.js
@@ -92,15 +92,84 @@
             $mdDialog.cancel();
         };
 
-        function updateCounter() {
+        vm.counterData = {};
 
-            vm.counter = vm.counter + 1;
+        function updateCounter(index, config) {
+
+            vm.counterData[index] = config;
+
+            vm.counter = 0;
+
+            var keys = Object.keys(vm.counterData);
+
+            keys.forEach(function (key) {
+
+                if (vm.counterData[key]) {
+                    if (vm.counterData[key].hasOwnProperty('task_status')) {
+
+                        if (vm.counterData[key].task_status === 'SUCCESS') {
+                            vm.counter = vm.counter + 1;
+                        }
+
+                    } else {
+                        vm.counter = vm.counter + 1;
+                    }
+                }
+
+            });
 
             $scope.$apply()
         }
 
+        vm.getTransactionImportProgress = function () {
+
+            var result = {};
+
+            var keys = Object.keys(vm.counterData);
+
+            var currentAction;
+            var currentActionIndex;
+
+            var schemeObject;
+
+            vm.schemes.forEach(function (scheme) {
+
+                if (scheme.id === vm.config.scheme) {
+                    schemeObject = scheme;
+                }
+
+            });
+
+            keys.forEach(function (key) {
+
+                if (vm.counterData[key].hasOwnProperty('error_handling') && vm.counterData[key].hasOwnProperty('task_status')) {
+
+                    if (vm.counterData[key].task_status !== 'SUCCESS') {
+                        currentAction = vm.counterData[key];
+                        currentActionIndex = key;
+                    }
+
+                }
+
+            });
+
+
+
+            if (currentAction && currentActionIndex && schemeObject) {
+
+                // console.log('currentActionIndex', currentActionIndex);
+
+                result.text = 'Processing ' + schemeObject.actions[currentActionIndex].action_notes + ':';
+                result.current = currentAction.processed_rows;
+                result.total = currentAction.total_rows;
+            }
+
+            return result;
+        };
 
         vm.validate = function ($event) {
+
+            vm.counterData = {};
 
             vm.readyStatus.processing = true;
 
@@ -109,7 +178,7 @@
             formData.append('file', vm.config.file);
             formData.append('scheme', vm.config.scheme);
 
-            console.log('vm.config', vm.config);
+            // console.log('vm.config', vm.config);
 
             var schemeObject;
 
@@ -145,7 +214,10 @@
                             return {
                                 current: vm.counter,
                                 total: vm.activeItemTotal,
-                                status: vm.status
+                                status: vm.status,
+                                additional: [
+                                    vm.getTransactionImportProgress()
+                                ]
                             }
                         }
                     }
@@ -155,7 +227,7 @@
 
             complexImportValidateService.validateImport(vm.config.file, vm.config.delimiter, schemeObject, updateCounter).then(function (data) {
 
-                console.log('data', data);
+                // console.log('data', data);
 
                 vm.status = 'SUCCESS';
 
@@ -215,7 +287,7 @@
             formData.append('file', vm.config.file);
             formData.append('scheme', vm.config.scheme);
 
-            console.log('vm.config', vm.config);
+            // console.log('vm.config', vm.config);
 
             var schemeObject;
 
@@ -227,10 +299,12 @@
 
             });
 
+            vm.counterData = {};
             vm.status = 'PROGRESS';
             vm.state = 'validate';
             vm.counter = 0;
             vm.activeItemTotal = schemeObject.actions.length;
+
 
             $mdDialog.show({
                 controller: 'LoaderDialogController as vm',
@@ -245,7 +319,7 @@
                         isCancelable: false,
                         current: vm.counter,
                         total: vm.activeItemTotal,
-                        text: 'Import Progress:',
+                        text: 'Validation Progress:',
                         status: vm.status,
                         callback: function () {
                             return {
@@ -261,7 +335,9 @@
 
             complexImportValidateService.validateImport(vm.config.file, vm.config.delimiter, schemeObject, updateCounter).then(function (data) {
 
-                console.log('validation data', data);
+                // console.log('validation data', data);
+
+                vm.status = 'SUCCESS';
 
                 var errorsCount = 0;
 
@@ -275,99 +351,137 @@
                     vm.counter = 0;
                     vm.activeItemTotal = schemeObject.actions.length;
 
-                    complexImportService.startImport(vm.config.file, vm.config.delimiter, schemeObject, updateCounter).then(function (data) {
+                    setTimeout(function () {
 
-                        console.log('data', data);
-
-                        vm.status = 'SUCCESS';
-                        vm.readyStatus.processing = false;
-
-                        errorsCount = 0;
-
-                        data.errors.forEach(function (error) {
-                            errorsCount = errorsCount + error.length;
-                        });
-
-                        if (errorsCount === 0) {
-
-                            $mdDialog.hide();
-
-                            var description = '';
-
-                            description = '<div>' +
-                                '<div>Imports in total: ' + schemeObject.actions.length + '</div>' +
-                                '</div><br/>';
-
-                            console.log('description', description);
-
-                            description = description + '<div> You have successfully imported file </div>';
-
-                            $mdDialog.show({
-                                controller: 'SuccessDialogController as vm',
-                                templateUrl: 'views/dialogs/success-dialog-view.html',
-                                targetEvent: $event,
-                                preserveScope: true,
-                                multiple: true,
-                                autoWrap: true,
-                                skipHide: true,
-                                locals: {
-                                    success: {
-                                        title: "Success",
-                                        description: description
-                                    }
-                                }
-
-                            });
-
-                        } else {
-
-                            $mdDialog.show({
-                                controller: 'ComplexImportValidationErrorsDialogController as vm',
-                                templateUrl: 'views/dialogs/complex-import/complex-import-validation-errors-dialog-view.html',
-                                targetEvent: $event,
-                                preserveScope: true,
-                                multiple: true,
-                                autoWrap: true,
-                                skipHide: true,
-                                locals: {
-                                    data: {
-                                        complexImportScheme: schemeObject,
-                                        validationResults: data
-                                    }
-                                }
-                            })
-
-                        }
-
-
-                    }).catch(function (reason) {
-
-                        console.log('here? ', reason);
-
-                        vm.readyStatus.processing = false;
+                        vm.status = 'PROGRESS';
 
                         $mdDialog.show({
-                            controller: 'ValidationDialogController as vm',
-                            templateUrl: 'views/dialogs/validation-dialog-view.html',
+                            controller: 'LoaderDialogController as vm',
+                            templateUrl: 'views/dialogs/loader-dialog-view.html',
                             targetEvent: $event,
                             preserveScope: true,
                             multiple: true,
                             autoWrap: true,
                             skipHide: true,
                             locals: {
-                                validationData: {
-                                    message: "An error occurred. Please try again later"
+                                data: {
+                                    isCancelable: false,
+                                    current: vm.counter,
+                                    total: vm.activeItemTotal,
+                                    text: 'Import Progress:',
+                                    status: vm.status,
+                                    callback: function () {
+                                        return {
+                                            current: vm.counter,
+                                            total: vm.activeItemTotal,
+                                            status: vm.status
+                                        }
+                                    }
                                 }
                             }
+
                         });
 
-                        $scope.$apply();
+                        vm.counterData = {};
 
-                    })
+                        complexImportService.startImport(vm.config.file, vm.config.delimiter, schemeObject, updateCounter).then(function (data) {
+
+                            // console.log('data', data);
+
+                            vm.status = 'SUCCESS';
+                            vm.readyStatus.processing = false;
+
+                            errorsCount = 0;
+
+                            data.errors.forEach(function (error) {
+                                errorsCount = errorsCount + error.length;
+                            });
+
+                            if (errorsCount === 0) {
+
+                                $mdDialog.hide();
+
+                                var description = '';
+
+                                description = '<div>' +
+                                    '<div>Imports in total: ' + schemeObject.actions.length + '</div>' +
+                                    '</div><br/>';
+
+                                // console.log('description', description);
+
+                                description = description + '<div> You have successfully imported file </div>';
+
+                                $mdDialog.show({
+                                    controller: 'SuccessDialogController as vm',
+                                    templateUrl: 'views/dialogs/success-dialog-view.html',
+                                    targetEvent: $event,
+                                    preserveScope: true,
+                                    multiple: true,
+                                    autoWrap: true,
+                                    skipHide: true,
+                                    locals: {
+                                        success: {
+                                            title: "Success",
+                                            description: description
+                                        }
+                                    }
+
+                                });
+
+                            } else {
+
+                                $mdDialog.show({
+                                    controller: 'ComplexImportValidationErrorsDialogController as vm',
+                                    templateUrl: 'views/dialogs/complex-import/complex-import-validation-errors-dialog-view.html',
+                                    targetEvent: $event,
+                                    preserveScope: true,
+                                    multiple: true,
+                                    autoWrap: true,
+                                    skipHide: true,
+                                    locals: {
+                                        data: {
+                                            complexImportScheme: schemeObject,
+                                            validationResults: data
+                                        }
+                                    }
+                                })
+
+                            }
+
+
+                        }).catch(function (reason) {
+
+                            // console.log('here? ', reason);
+
+                            vm.readyStatus.processing = false;
+
+                            vm.status = 'SUCCESS';
+
+                            $mdDialog.show({
+                                controller: 'ValidationDialogController as vm',
+                                templateUrl: 'views/dialogs/validation-dialog-view.html',
+                                targetEvent: $event,
+                                preserveScope: true,
+                                multiple: true,
+                                autoWrap: true,
+                                skipHide: true,
+                                locals: {
+                                    validationData: {
+                                        message: "An error occurred. Please try again later"
+                                    }
+                                }
+                            });
+
+                            $scope.$apply();
+
+                        })
+
+                    }, 500)
 
                 } else {
 
                     vm.readyStatus.processing = false;
+                    vm.status = 'SUCCESS';
 
                     $mdDialog.show({
                         controller: 'ComplexImportValidationErrorsDialogController as vm',
