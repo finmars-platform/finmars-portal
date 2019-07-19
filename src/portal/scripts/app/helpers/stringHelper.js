2ea3a7e5f06d59e38d7392fff4abbaab60503064
szhitenev <szhitenev>
2019-07-19 19:30:55 +0300

    test2

--- src/portal/scripts/app/helpers/stringHelper.js
+++ src/portal/scripts/app/helpers/stringHelper.js
@@ -6,10 +6,12 @@
     'use strict';
 
     var md5Helper = require('./md5.helper');
+    var sha1Helper = require('./sha1.helper');
 
     var toHash = function (str) {
 
-        return md5Helper.md5(str);
+        // return md5Helper.md5(str);
+        return sha1Helper.sha1(str);
     };
 
     module.exports = {
