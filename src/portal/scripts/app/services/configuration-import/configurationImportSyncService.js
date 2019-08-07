0a719735d770910641460870218dbdb6ef658089
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-08-07 12:57:55 +0300

    sync

--- src/portal/scripts/app/services/configuration-import/configurationImportSyncService.js
+++ src/portal/scripts/app/services/configuration-import/configurationImportSyncService.js
@@ -403,7 +403,7 @@
                                         item: errorOptions.item,
                                         content_type: errorOptions.content_type,
                                         error: {
-                                            message: "Can't find Transaction Type input for fields. Missing Input names: " + fieldsError.join(', ')
+                                            message: "Can't find Transaction Type (" + rule.___transaction_type__user_code + " # " + rule._order+ ") input for fields. Missing Input names: " + fieldsError.join(', ')
                                         },
                                         mode: 'The related Transaction Type has been deleted from layout'
 
