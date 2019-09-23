87e3f50452dc157b36a7d8b8f40d3f3fbc5f9aa0
szhitenev <szhitenev>
2019-09-23 19:45:44 +0300

    fix

--- src/portal/scripts/app/services/entityUrlService.js
+++ src/portal/scripts/app/services/entityUrlService.js
@@ -37,7 +37,8 @@
             case 'transaction-type-group':
                 return 'transactions/transaction-type-group';
             case 'complex-transaction':
-                return 'transactions/complex-transaction';
+                // return 'transactions/complex-transaction';
+                return 'transactions/complex-transaction-light';
             case 'currency':
                 return 'currencies/currency';
             case 'currency-history':
