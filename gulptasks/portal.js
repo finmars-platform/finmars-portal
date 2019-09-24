2c1d812681018e970def8f17687dd5d0c62cd78b
Sergei Zhitenev <s.zhitenev@finmars.com>
2019-09-24 18:51:50 +0300

    fix gulp

--- gulptasks/portal.js
+++ gulptasks/portal.js
@@ -146,8 +146,8 @@
             .pipe(replace(/__PROJECT_ENV__/g, PROJECT_ENV))
             .pipe(replace(/__LOGIN__/g, credentials[PROJECT_ENV].login))
             .pipe(replace(/__PASS__/g, credentials[PROJECT_ENV].pass))
-            // .pipe(gulpif(PROJECT_ENV === 'production', uglify()))
-            .pipe(uglify()) // if you need to debug minified build locally
+            .pipe(gulpif(PROJECT_ENV === 'production', uglify()))
+            // .pipe(uglify()) // if you need to debug minified build locally
             .pipe(rename({basename: 'main', suffix: '.min'}))
             .on('error', function (error) {
                 console.error('\nError on JS minification: \n', error.toString());
