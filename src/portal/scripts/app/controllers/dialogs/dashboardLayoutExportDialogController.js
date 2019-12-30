447fdf076b8704f41152386e75fbf8b0f8e3a882
szhitenev <szhitenev>
2019-12-30 13:19:00 +0300

    update

--- src/portal/scripts/app/controllers/dialogs/dashboardLayoutExportDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/dashboardLayoutExportDialogController.js
@@ -5,6 +5,8 @@
 
     'use strict';
 
+    var uiService = require('../../services/uiService');
+
     module.exports = function ($scope, $mdDialog, data) {
 
         var vm = this;
@@ -24,51 +26,174 @@
             $mdDialog.hide({status: 'disagree'});
         };
 
+        vm.getAllReportLayouts = function () {
+
+            return new Promise(function (resolve, reject) {
+
+
+                var promises = [];
+
+                promises.push(uiService.getListLayoutDefault(
+                    {
+                        filters: {
+                            content_type: 'reports.balancereport'
+                        }
+                    }
+                ));
+
+                promises.push(uiService.getListLayoutDefault(
+                    {
+                        filters: {
+                            content_type: 'reports.plreport'
+                        }
+                    }
+                ));
+
+                promises.push(uiService.getListLayoutDefault(
+                    {
+                        filters: {
+                            content_type: 'reports.transactionreport'
+                        }
+                    }
+                ));
+
+
+                Promise.all(promises).then(function (data) {
+
+                    console.log('data', data);
+
+                    var result = [];
+
+                    data.forEach(function (dataItem) {
+                        dataItem.results.forEach(function (layout) {
+                            result.push(layout)
+                        })
+                    });
+
+                    console.log('result', result);
+
+                    resolve(result)
+
+                })
+
+            })
+
+        };
+
+        vm.getReportLayoutsFromDashboardLayout = function (dashboardLayout) {
+
+            return new Promise(function (resolve, reject) {
+
+                vm.getAllReportLayouts().then(function (data) {
+
+                    var layouts = data;
+
+                    var result = [];
+                    var addedLayouts = {};
+
+
+                    dashboardLayout.data.components_types.forEach(function (component) {
+
+
+                        if (['report_viewer', 'report_viewer_matrix',
+                            'report_viewer_split_panel', 'report_viewer_bars_chart'].indexOf(component.type) !== -1) {
+
+
+                            layouts.forEach(function (layout) {
+
+
+                                if (component.settings.layout_name === layout.name && component.settings.content_type === layout.content_type) {
+
+                                    delete layout.id;
+                                    delete layout.is_default;
+                                    delete layout.is_active;
+
+                                    if (!addedLayouts.hasOwnProperty(layout.content_type)) {
+                                        addedLayouts[layout.content_type] = [];
+                                    }
+
+                                    if (addedLayouts[layout.content_type].indexOf(layout.name) === -1) {
+
+                                        addedLayouts[layout.content_type].push(layout.name);
+
+                                        result.push(layout);
+
+                                    }
+                                }
+
+
+                            })
+                        }
+
+
+                    });
+
+
+                    resolve(result);
+
+                });
+
+            })
+
+        };
+
         vm.setDownloadLink = function () {
 
             var link = document.querySelector('.export-classifier-link');
 
-            var contentType = 'ui.dashboardlayout';
+            var date = new Date().toISOString().slice(0, 10); // yyyy-mm-dd
 
-            var date = new Date().toISOString().slice(0,10); // yyyy-mm-dd
+            vm.getReportLayoutsFromDashboardLayout(vm.layout).then(function (data) {
 
-            var configuration = {
-                "head":{
-                    "date": date
-                },
-                "body":[
-                    {
-                        "section_name":"configuration",
-                        "items":[
-                            {
-                                "entity": contentType,
-                                "content":[
-                                    vm.layout
-                                ],
-                                "count": 1
-                            }
-                        ]
+                var reportLayouts = data;
+
+                console.log('reportLayouts', reportLayouts);
+
+                var configuration = {
+                    "head": {
+                        "date": date
+                    },
+                    "body": [
+                        {
+                            "section_name": "configuration",
+                            "items": [
+                                {
+                                    "entity": "ui.dashboardlayout",
+                                    "content": [
+                                        vm.layout
+                                    ],
+                                    "count": 1
+                                },
+                                {
+                                    "entity": "ui.reportlayout",
+                                    "content": reportLayouts,
+                                    "count": reportLayouts.length
+                                }
+                            ]
+                        }
+                    ]
+                };
+
+
+                var text = JSON.stringify(configuration);
+
+                var file = new Blob([text], {type: 'text/plain'});
+
+                link.href = URL.createObjectURL(file);
+
+                link.addEventListener('click', function () {
+
+                    if (vm.filename) {
+                        link.download = vm.filename + '.fcfg';
+                    } else {
+                        link.download = 'layout.fcfg';
                     }
-                ]
-            };
 
+                    $mdDialog.hide();
+                })
 
-            var text = JSON.stringify(configuration);
-
-            var file = new Blob([text], {type: 'text/plain'});
-
-            link.href = URL.createObjectURL(file);
-
-            link.addEventListener('click', function () {
-
-                if (vm.filename) {
-                    link.download = vm.filename + '.fcfg';
-                } else {
-                    link.download = 'layout.fcfg';
-                }
-
-                $mdDialog.hide();
             })
+
 
         };
 
