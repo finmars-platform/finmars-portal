c07d8f7ee40bedad8e84b29a4256230d42375f6f
Sergei Zhitenev <s.zhitenev@finmars.com>
2020-04-15 16:28:02 +0300

    fix

--- src/portal/scripts/app/controllers/dialogs/dashboard/dashboardLayoutListDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/dashboard/dashboardLayoutListDialogController.js
@@ -83,7 +83,8 @@
                 clickOutsideToClose: false,
                 locals: {
                     options: {
-                        layoutName: layoutData.name
+                        layoutName: layoutData.name,
+                        layoutUserCode: layoutData.user_code
                     }
                 }
 
