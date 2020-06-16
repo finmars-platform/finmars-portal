8ff4e7687c7a2ab45a83444b9cfbdebf093c68ac
szhitenev <szhitenev>
2020-06-16 22:15:45 +0300

    buttonset update

--- src/portal/scripts/app/directives/dashboard/dashboardButtonSetDirective.js
+++ src/portal/scripts/app/directives/dashboard/dashboardButtonSetDirective.js
@@ -2,10 +2,15 @@
 
     'use strict';
 
-    var dashboardEvents = require('../../services/dashboard/dashboardEvents')
-    var dashboardComponentStatuses = require('../../services/dashboard/dashboardComponentStatuses')
+    var dashboardEvents = require('../../services/dashboard/dashboardEvents');
+    var dashboardComponentStatuses = require('../../services/dashboard/dashboardComponentStatuses');
 
-    module.exports = function () {
+    var transactionTypeService = require('../../services/transactionTypeService');
+    var pricingProcedureService = require('../../services/pricing/pricingProcedureService');
+
+    var toastNotificationService = require('../../../../../core/services/toastNotificationService');
+
+    module.exports = function dashboardButtonSetDirective($mdDialog) {
         return {
             restriction: 'E',
             templateUrl: 'views/directives/dashboard/dashboard-button-set-view.html',
@@ -19,51 +24,14 @@
             },
             link: function (scope, elem, attr) {
 
-                scope.grid = [];
 
-                scope.buttons = scope.item.data.settings.buttons;
-
-                scope.getButtonSet = function(){
-
-                    scope.rows = scope.item.data.settings.rows;
-                    scope.columns = scope.item.data.settings.columns;
-
-                    var index = 0;
-
-                    if (scope.buttons.length) {
-
-                        for (var r = 0; r < scope.rows; r = r + 1) {
-
-                            scope.grid[r] = {
-                                columns: []
-                            };
-
-                            for (var c = 0; c < scope.columns; c = c + 1) {
-
-                                if (scope.buttons[index]) {
-                                    scope.grid[r].columns[c] = scope.buttons[index];
-                                }
-
-                                index = index + 1;
-
-                            }
-
-                        }
-
-                    }
-
-                    console.log('grid', scope.grid);
-
-
-                };
-
-                scope.initEventListeners = function(){
+                scope.initEventListeners = function () {
 
                     scope.dashboardEventService.addEventListener(dashboardEvents.COMPONENT_STATUS_CHANGE, function () {
 
                         var status = scope.dashboardDataService.getComponentStatus(scope.item.data.id);
 
-                        if(status === dashboardComponentStatuses.START) { // No actual calculation happens, so set to Active state
+                        if (status === dashboardComponentStatuses.START) { // No actual calculation happens, so set to Active state
                             scope.dashboardDataService.setComponentStatus(scope.item.data.id, dashboardComponentStatuses.ACTIVE);
                             scope.dashboardEventService.dispatchEvent(dashboardEvents.COMPONENT_STATUS_CHANGE);
                         }
@@ -74,12 +42,125 @@
 
                 scope.init = function () {
 
-                    scope.getButtonSet();
+                    scope.componentData = scope.dashboardDataService.getComponentById(scope.item.data.id);
+
+                    scope.rows = scope.componentData.settings.rows;
+                    scope.columns = scope.componentData.settings.columns;
+                    scope.grid = scope.componentData.settings.grid;
+
+                    console.log('scope.grid', scope.grid);
 
                     scope.dashboardDataService.setComponentStatus(scope.item.data.id, dashboardComponentStatuses.INIT);
                     scope.dashboardEventService.dispatchEvent(dashboardEvents.COMPONENT_STATUS_CHANGE);
 
                     scope.initEventListeners();
+
+                };
+
+                scope.handleAction = function ($event, item) {
+
+                    console.log('handleAction $event', $event);
+                    console.log('handleAction item', item);
+
+                    if (item.action === 'book_transaction') {
+
+                        transactionTypeService.getListLight({
+                            filters: {
+                                user_code: item.target
+                            }
+                        }).then(function (data) {
+
+                            if (data.results.length) {
+
+                                var transactionType = data.results[0];
+
+                                var contextData = {};
+
+                                $mdDialog.show({
+                                    controller: 'ComplexTransactionAddDialogController as vm',
+                                    templateUrl: 'views/entity-viewer/complex-transaction-add-dialog-view.html',
+                                    parent: angular.element(document.body),
+                                    targetEvent: $event,
+                                    locals: {
+                                        entityType: 'complex-transaction',
+                                        entity: {
+                                            contextData: contextData,
+                                            transaction_type: transactionType.id
+                                        }
+                                    }
+                                }).then(function (res) {
+
+
+                                })
+
+                            } else {
+                                toastNotificationService.error('Transaction Type is not found');
+                            }
+
+                        })
+
+                    }
+
+                    if (item.action === 'create_new_record') {
+
+                        $mdDialog.show({
+                            controller: 'EntityViewerAddDialogController as vm',
+                            templateUrl: 'views/entity-viewer/entity-viewer-add-dialog-view.html',
+                            parent: angular.element(document.body),
+                            targetEvent: $event,
+                            locals: {
+                                entityType: item.target,
+                                entity: {}
+                            }
+                        }).then(function (res) {
+
+
+                        })
+
+                    }
+
+                    if (item.action === 'run_valuation_procedure') {
+
+                        pricingProcedureService.getList({
+                            filters: {
+                                user_code: item.target
+                            }
+                        }).then(function (data) {
+
+                            if(data.results.length) {
+
+                                var procedure = data.results[0];
+
+                                pricingProcedureService.runProcedure(procedure.id, procedure).then(function (data) {
+
+                                    $mdDialog.show({
+                                        controller: 'InfoDialogController as vm',
+                                        templateUrl: 'views/info-dialog-view.html',
+                                        parent: angular.element(document.body),
+                                        targetEvent: $event,
+                                        clickOutsideToClose: false,
+                                        preserveScope: true,
+                                        autoWrap: true,
+                                        skipHide: true,
+                                        multiple: true,
+                                        locals: {
+                                            info: {
+                                                title: 'Success',
+                                                description: "Procedure is being processed"
+                                            }
+                                        }
+                                    });
+
+                                })
+
+                            } else {
+                                toastNotificationService.error('Pricing Procedure is not found');
+                            }
+
+
+                        })
+
+                    }
 
                 };
 
