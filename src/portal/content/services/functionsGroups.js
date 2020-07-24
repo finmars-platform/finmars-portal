8d97fa7c71e9eca0696eb1011b4c7d9b20af67f4
mevstratov <m.evstratov@finmars.com>
2020-07-24 17:01:46 +0300

    input with expression validation

---
+++ src/portal/content/services/functionsGroups.js
@@ -0,0 +1,66 @@
+(function () {
+
+    var functionsGroups = [
+        {
+            "name": "All",
+            "key": "all"
+        },
+        {
+            "name": "Date",
+            "key": "date"
+        },
+        {
+            "name": "Number",
+            "key": "number"
+        },
+        {
+            "name": "Text",
+            "key": "text"
+        },
+        {
+            "name": "Instrument Formulas",
+            "key": "instr_formulas"
+        },
+        {
+            "name": "Instrument",
+            "key": "instr"
+        },
+        {
+            "name": "Account",
+            "key": "account"
+        },
+        {
+            "name": "Portfolio",
+            "key": "portfolio"
+        },
+        {
+            "name": "Responsible",
+            "key": "responsible"
+        },
+        {
+            "name": "Counterparty",
+            "key": "counterparty"
+        },
+        {
+            "name": "Currency",
+            "key": "currency"
+        },
+        {
+            "name": "Transaction description elements",
+            "key": "transact_des"
+        },
+        {
+            "name": "Data Types",
+            "key": "data_types"
+        },
+        {
+            "name": "Context Variables",
+            "key": "context_var"
+        }
+    ];
+
+    module.exports = {
+        functionsGroups: functionsGroups
+    }
+
+}());
