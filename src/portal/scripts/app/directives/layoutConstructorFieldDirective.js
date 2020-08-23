0880011e75da5bfba844adffc7db812c8a7a2e2c
mevstratov <m.evstratov@finmars.com>
2020-08-23 22:03:18 +0300

    color picker fixes

--- src/portal/scripts/app/directives/layoutConstructorFieldDirective.js
+++ src/portal/scripts/app/directives/layoutConstructorFieldDirective.js
@@ -72,13 +72,13 @@
                     if (scope.item.backgroundColor) {
 
                         scope.fieldUsesBackgroundColor = true;
-                        //scope.fieldBackgroundColor = scope.item.backgroundColor;
                         scope.backgroundColor.color = scope.item.backgroundColor;
 
                     } else {
+
                         scope.fieldUsesBackgroundColor = false;
-                        //scope.fieldBackgroundColor = '#000000';
                         scope.backgroundColor.color = {};
+
                     }
 
                     if (scope.item.attribute && scope.item.attribute.value_type !== "decoration") {
@@ -609,7 +609,21 @@
                 };
 
                 scope.toggleBackgroundColor = function () {
+
                     scope.fieldUsesBackgroundColor = !scope.fieldUsesBackgroundColor;
+
+                    if (scope.fieldUsesBackgroundColor) {
+
+                        if (!scope.backgroundColor || !Object.keys(scope.backgroundColor.color).length) {
+
+                            scope.backgroundColor = {
+                                color: {colorOrder: 0, paletteUserCode: "Default Palette"}
+                            };
+
+                        }
+
+                    }
+
                 };
 
                 /*scope.setFieldBackgroundColor = function (color) {
