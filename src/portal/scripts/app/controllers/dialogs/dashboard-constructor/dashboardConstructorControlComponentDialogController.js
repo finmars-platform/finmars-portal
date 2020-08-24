3ec8c00aae2fdf80d1af99ac806b2ab8cdb0c899
zubr <zubr@rambler.ru>
2020-08-24 13:47:41 +0300

    default value for date

--- src/portal/scripts/app/controllers/dialogs/dashboard-constructor/dashboardConstructorControlComponentDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/dashboard-constructor/dashboardConstructorControlComponentDialogController.js
@@ -5,9 +5,13 @@
 
     'use strict';
 
+    var uiService = require('../../../services/uiService');
+    var evRvLayoutsHelper = require('../../../helpers/evRvLayoutsHelper');
+
     module.exports = function ($scope, $mdDialog, item, dataService, eventService) {
 
         var vm = this;
+        vm.processing = false;
 
         if (item) {
             vm.item = item;
@@ -67,6 +71,13 @@
             }
         ];
 
+        vm.defaultValue = {
+            mode: 2,
+            entity_type: null,
+            layout: null,
+            report_field: {},
+            setValue: null
+        };
 
         vm.cancel = function () {
             $mdDialog.hide({status: 'disagree'});
@@ -78,14 +89,6 @@
 
             if (vm.item.id) {
 
-                /*vm.componentsTypes = vm.componentsTypes.map(function (item) {
-
-                    if (item.id === vm.item.id) {
-                        return vm.item
-                    }
-
-                    return item;
-                })*/
                 dataService.updateComponentById(vm.item);
 
             } else {
@@ -105,10 +108,105 @@
 
         vm.init = function () {
 
-            console.log('dataService', dataService);
-
             vm.componentsTypes = dataService.getComponents()
 
+        };
+
+        vm.isRequiredDefaultValue = function () {
+            var dateValueType = 40;
+            var contentTypesThatNeedsDefaultValues = [
+                'portfolios.portfolio',
+                'accounts.account',
+                'currencies.currency',
+                'strategies.strategy1',
+                'strategies.strategy2',
+                'strategies.strategy3',
+                'instruments.pricingpolicy'
+            ];
+
+            return vm.item.settings.value_type === dateValueType || contentTypesThatNeedsDefaultValues.includes(vm.item.settings.content_type);
+
+        };
+
+        vm.reportTypeChange = function() {
+
+            vm.defaultValue.layout = null;
+            vm.defaultValue.report_field = null;
+
+            vm.processing = true;
+
+            uiService.getListLayout(vm.defaultValue.entity_type).then(function (data) {
+
+                var layouts = data.results;
+                vm.layoutsWithLinkToFilters = evRvLayoutsHelper.getDataForLayoutSelectorWithFilters(layouts);
+                vm.processing = false;
+
+                $scope.$apply();
+
+            }).catch(function() {
+                vm.processing = false;
+            });
+
+        };
+
+
+        vm.onRvLayoutChange = function () {
+
+            if (!vm.defaultValue.layout) {
+                return;
+            }
+
+            vm.processing = true;
+            $scope.$apply();
+
+            vm.defaultValue.report_field = null;
+
+            var layoutId = vm.defaultValue.layout;
+
+            uiService.getListLayoutByKey(layoutId).then(function (layout) {
+
+                vm.reportFields = getReportFields(vm.defaultValue.entity_type, layout);
+
+                if (vm.defaultValue.entity_type === 'balance-report') {
+
+                    vm.defaultValue.report_field = vm.reportFields[0];
+
+                }
+
+                vm.processing = false;
+
+                $scope.$apply();
+
+            }).catch(function() {
+                vm.processing = false;
+            });
+
+        };
+
+
+        var getReportFields = function (reportType, layout) {
+
+            var defaultValueReportFields = {
+                'balance-report': [
+                    {key: 'report_date', name: 'Date'}
+                ],
+                'pl-report': [
+                    {key: 'pl_first_date', name: 'Date from (excl)'},
+                    {key: 'report_date', name: 'Date to (incl)'}
+                ],
+                'transaction-report': [
+                    {key: 'begin_date', name: 'Date from (incl)'},
+                    {key: 'end_date', name: 'Date to (incl)'}
+                ]
+            };
+
+            return defaultValueReportFields[reportType].map(function (field) {
+                return {
+                    key: field.key,
+                    name: field.name,
+                    value: layout.data.reportOptions[field.key]
+                }
+            })
         };
 
         vm.init()
