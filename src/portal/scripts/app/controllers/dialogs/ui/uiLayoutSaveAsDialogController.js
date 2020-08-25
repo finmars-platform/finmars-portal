2f155d839d2bf7ef44c4e1b6438cdc614f7ec489
AndreyPanovA <panov3107@mail.ru>
2020-08-25 16:43:47 +0300

    inputs/bugs/double

--- src/portal/scripts/app/controllers/dialogs/ui/uiLayoutSaveAsDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/ui/uiLayoutSaveAsDialogController.js
@@ -3,95 +3,95 @@
  */
 (function () {
 
-    'use strict';
+  'use strict';
 
-    var logService = require('../../../../../../core/services/logService');
+  var logService = require('../../../../../../core/services/logService');
 
-    var uiService = require('../../../services/uiService');
+  var uiService = require('../../../services/uiService');
 
-    module.exports = function ($scope, $mdDialog, options) {
+  module.exports = function ($scope, $mdDialog, options) {
 
-        logService.controller('UiLayoutSaveAsDialogController', 'initialized');
+    logService.controller('UiLayoutSaveAsDialogController', 'initialized');
 
-        var vm = this;
+    var vm = this;
 
-        vm.complexSaveAsLayoutDialog = false;
+    vm.complexSaveAsLayoutDialog = false;
 
-        var layoutsUserCodes = ["New Layout"];
+    var layoutsUserCodes = ["New Layout"];
 
-        if (options) {
+    if (options) {
 
-            if (options.complexSaveAsLayoutDialog) {
+      if (options.complexSaveAsLayoutDialog) {
 
-                vm.complexSaveAsLayoutDialog = true;
-                vm.entityType = options.complexSaveAsLayoutDialog.entityType;
+        vm.complexSaveAsLayoutDialog = true;
+        vm.entityType = options.complexSaveAsLayoutDialog.entityType;
 
-                uiService.getListLayout(vm.entityType).then(function (data) {
+        uiService.getListLayout(vm.entityType).then(function (data) {
 
-                    var layouts = data.results;
+          var layouts = data.results;
 
-                    layouts.map(function (layout) {
-                        layoutsUserCodes.push(layout.user_code);
-                    });
+          layouts.map(function (layout) {
+            layoutsUserCodes.push(layout.user_code);
+          });
 
-                });
+        });
 
-            }
+      }
 
-            if (options.layoutName) {
+      if (options.layoutName) {
 
-                vm.layoutName = options.layoutName;
+        vm.layoutName = options.layoutName;
 
-            }
+      }
 
-            if (options.layoutUserCode) {
-                vm.layoutUserCode = options.layoutUserCode;
-            }
-
-        }
-
-        vm.cancel = function () {
-            $mdDialog.hide({status: 'disagree'});
-        };
-
-        vm.agree = function ($event) {
-
-            var layoutNameOccupied = false;
-
-            if (vm.complexSaveAsLayoutDialog) {
-
-                var i;
-                for (i = 0; i < layoutsUserCodes.length; i++) {
-
-                    if (layoutsUserCodes[i] === vm.layoutUserCode) {
-                        layoutNameOccupied = true;
-
-                        $mdDialog.show({
-                            controller: 'WarningDialogController as vm',
-                            templateUrl: 'views/warning-dialog-view.html',
-                            parent: angular.element(document.body),
-                            targetEvent: $event,
-                            clickOutsideToClose: false,
-                            multiple: true,
-                            locals: {
-                                warning: {
-                                    title: 'Warning',
-                                    description: 'There is already layout with such user code. Layouts should have unique user codes.'
-                                }
-                            }
-                        });
-
-                        break;
-                    }
-                }
-            }
-
-            if (!layoutNameOccupied) {
-                $mdDialog.hide({status: 'agree', data: {name: vm.layoutName, user_code: vm.layoutUserCode}});
-            }
-
-        };
+      if (options.layoutUserCode) {
+        vm.layoutUserCode = options.layoutUserCode;
+      }
 
     }
+
+    vm.cancel = function () {
+      $mdDialog.hide({ status: 'disagree' });
+    };
+
+    vm.agree = function ($event) {
+
+      var layoutNameOccupied = false;
+
+      if (vm.complexSaveAsLayoutDialog) {
+
+        var i;
+        for (i = 0; i < layoutsUserCodes.length; i++) {
+
+          if (layoutsUserCodes[i] === vm.layoutUserCode) {
+            layoutNameOccupied = true;
+
+            $mdDialog.show({
+              controller: 'WarningDialogController as vm',
+              templateUrl: 'views/warning-dialog-view.html',
+              parent: angular.element(document.body),
+              targetEvent: $event,
+              clickOutsideToClose: false,
+              multiple: true,
+              locals: {
+                warning: {
+                  title: 'Warning',
+                  description: 'There is already layout with such user code. Layouts should have unique user codes.'
+                }
+              }
+            });
+
+            break;
+          }
+        }
+      }
+
+      if (!layoutNameOccupied) {
+        $mdDialog.hide({ status: 'agree', data: { name: vm.layoutName, user_code: vm.layoutUserCode } });
+      }
+
+    };
+
+  }
 
 }());
