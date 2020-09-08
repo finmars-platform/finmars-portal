2d7b64b253df1a5edbcd05e721f71efffad9eb2b
vzubr <zubr@rambler.ru>
2020-09-08 09:17:38 +0300

    loader for charts and matrix

--- src/portal/scripts/app/directives/dashboard/dashboardReportViewerChartsDirective.js
+++ src/portal/scripts/app/directives/dashboard/dashboardReportViewerChartsDirective.js
@@ -26,7 +26,8 @@
             link: function (scope, elem, attr) {
 
                 scope.readyStatus = {
-                    data: 'processing'
+                    data: 'processing',
+                    disabled: false
                 };
 
                 scope.dashboardComponentDataService = new DashboardComponentDataService;
@@ -245,6 +246,19 @@
                         });
 
                     }
+
+
+                    scope.dashboardComponentEventService.addEventListener(dashboardEvents.COMPONENT_BLOCKAGE_ON, function () {
+
+                        scope.readyStatus.disabled = true;
+
+                    });
+
+                    scope.dashboardComponentEventService.addEventListener(dashboardEvents.COMPONENT_BLOCKAGE_OFF, function () {
+
+                        scope.readyStatus.disabled = false;
+
+                    });
 
                 };
 
