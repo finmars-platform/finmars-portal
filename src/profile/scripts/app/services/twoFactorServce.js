22d577fe9377c7833b397133ff780cd8407d2a51
vzubr <zubr@rambler.ru>
2020-09-08 20:58:34 +0300

    set is_active attribute when device added successful

--- src/profile/scripts/app/services/twoFactorServce.js
+++ src/profile/scripts/app/services/twoFactorServce.js
@@ -71,12 +71,28 @@
         })
     };
 
+    var activateTwoFactor = function (id) {
+        return window.fetch(baseUrl + 'users/two-factor/' + id + '/', {
+            method: 'PATCH',
+            credentials: 'include',
+            headers: {
+                'X-CSRFToken': cookieService.getCookie('csrftoken'),
+                Accept: 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({id: id, is_active: true})
+        }).then(function (data) {
+            return data.json();
+        })
+    };
+
 
     module.exports = {
         getList: getList,
         generateCode: generateCode,
         validateCode: validateCode,
-        deleteByKey: deleteByKey
+        deleteByKey: deleteByKey,
+        activateTwoFactor: activateTwoFactor
     }
 
 }());
