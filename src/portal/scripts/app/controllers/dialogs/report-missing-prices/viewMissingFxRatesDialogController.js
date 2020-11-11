250ecc7a8790d72fcc43bbdfffc87ff981f199b3
szhitenev <szhitenev>
2020-11-11 14:00:40 +0300

    hotfix: missing prices update

--- src/portal/scripts/app/controllers/dialogs/report-missing-prices/viewMissingFxRatesDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/report-missing-prices/viewMissingFxRatesDialogController.js
@@ -16,6 +16,46 @@
         vm.evDataService = vm.data.evDataService;
         vm.items = vm.data.items;
 
+        vm.viewCurrency = function($event, item) {
+
+            $mdDialog.show({
+                controller: 'EntityViewerEditDialogController as vm',
+                templateUrl: 'views/entity-viewer/entity-viewer-edit-dialog-view.html',
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                multiple: true,
+                locals: {
+                    entityType: 'currency',
+                    entityId: item.id,
+                    data: {}
+                }
+            })
+
+        };
+
+        vm.addFXRate = function($event, item) {
+
+            $mdDialog.show({
+                controller: 'EntityViewerAddDialogController as vm',
+                templateUrl: 'views/entity-viewer/entity-viewer-add-dialog-view.html',
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                multiple: true,
+                locals: {
+                    entityType: 'currency-history',
+                    entity: {
+                        currency: item.id,
+                        currency_object: item.currency_object,
+                        pricing_policy: vm.reportOptions.pricing_policy,
+                        pricing_policy_object: vm.reportOptions.pricing_policy_object,
+                        date: vm.reportOptions.report_date
+                    },
+                    data: {}
+                }
+            })
+
+        };
+
         vm.init = function () {
 
             vm.reportOptions = vm.evDataService.getReportOptions();
