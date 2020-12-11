0a6e259eb627fd3d130ec6c8448e1c1dbfb5aa3f
szhitenev <szhitenev>
2020-12-11 17:40:19 +0300

    new accordions

--- src/portal/scripts/app/controllers/dialogs/dashboard-constructor/dashboardConstructorAccordionEditorDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/dashboard-constructor/dashboardConstructorAccordionEditorDialogController.js
@@ -12,12 +12,154 @@
         vm.layout = data.layout;
 
         vm.cancel = function () {
+
+            vm.clearCanCreateRowStatus();
+            vm.clearProxyAccordions();
+
             $mdDialog.hide({status: 'disagree'});
         };
 
         vm.agree = function () {
 
+            vm.clearCanCreateRowStatus();
+            vm.clearProxyAccordions();
+
             $mdDialog.hide({status: 'agree', data: {layout: vm.layout}});
+        }
+
+        vm.clearProxyAccordions = function () {
+
+            vm.layout.data.tabs.forEach(function (tab) {
+
+                if (tab.accordions) {
+
+                    tab.accordions = tab.accordions.filter(function (item) {
+                        return item.type === 'accordion';
+                    })
+
+                    tab.accordions.forEach(function (accordion) {
+
+                        delete accordion.type;
+
+                    })
+
+                }
+
+            })
+
+        }
+
+        vm.updateProxyAccordions = function () {
+
+            vm.layout.data.tabs.forEach(function (tab) {
+
+                if (tab.accordions) {
+
+                    var newAccordions = [];
+
+                    tab.accordions.forEach(function (accordion) {
+
+                        accordion.type = 'accordion';
+
+                    })
+
+                    tab.layout.rows.forEach(function (row, index) {
+
+                        var isEmpty = true;
+                        var findedAccordion = null;
+
+                        tab.accordions.forEach(function (accordion) {
+
+                            if (index >= accordion.from && index <= accordion.to) {
+                                isEmpty = false;
+
+                                if (index !== accordion.from) {
+                                    findedAccordion = {
+                                        type: 'proxy'
+                                    };
+                                } else {
+                                    findedAccordion = accordion;
+                                }
+
+
+                            }
+
+                        });
+
+                        if (isEmpty) {
+                            newAccordions.push({
+                                type: 'proxy'
+                            })
+                        } else {
+                            newAccordions.push(findedAccordion)
+                        }
+
+                    })
+
+                    tab.accordions = newAccordions;
+
+
+                }
+
+            });
+
+        }
+
+        vm.clearCanCreateRowStatus = function () {
+
+            vm.layout.data.tabs.forEach(function (tab) {
+
+                tab.layout.rows.forEach(function (row) {
+
+                    delete row.canCreateAccordion;
+
+                })
+
+            })
+
+        }
+
+        vm.updateCanCreateRowStatus = function () {
+
+            vm.clearCanCreateRowStatus();
+
+
+            vm.layout.data.tabs.forEach(function (tab) {
+
+                var filledRows = [];
+
+                if (tab.accordions) {
+
+                    tab.accordions.forEach(function (accordion) {
+
+                        if (accordion.from === accordion.to) {
+                            filledRows.push(accordion.from);
+                        } else {
+
+                            for (var i = accordion.from; i <= accordion.to; i = i + 1) {
+                                filledRows.push(i);
+                            }
+
+                        }
+
+                    })
+
+                }
+
+                console.log('filledRows', filledRows);
+
+                tab.layout.rows.forEach(function (row, index) {
+
+                    if (filledRows.indexOf(index) === -1) {
+                        row.canCreateAccordion = true;
+                    } else {
+                        row.canCreateAccordion = false;
+                    }
+
+                });
+
+            })
+
         }
 
         vm.addAccordion = function ($event, tab, row, name, $index) {
@@ -25,8 +167,6 @@
             console.log('vm.layout', vm.layout);
             console.log('tab', tab);
             console.log('row', row);
-
-
 
             if (!tab.accordions) {
                 tab.accordions = []
@@ -41,46 +181,36 @@
 
             delete row.accordionName;
 
+            vm.updateCanCreateRowStatus();
+            vm.updateProxyAccordions();
+
         };
 
         vm.increaseAccordion = function ($event, item) {
             item.to = item.to + 1;
+            vm.updateCanCreateRowStatus();
+            vm.updateProxyAccordions();
         }
         vm.decreaseAccordion = function ($event, item) {
             item.to = item.to - 1;
+            vm.updateCanCreateRowStatus();
+            vm.updateProxyAccordions();
         }
 
         vm.deleteAccordion = function ($event, tab, $index) {
 
             tab.accordions.splice($index, 1);
 
+            vm.updateCanCreateRowStatus();
+
         }
-
-        vm.canCreateAccordion = function (tab, $index) {
-
-            var result = true;
-
-            if (tab.accordions) {
-
-                tab.accordions.forEach(function (item){
-
-                    if(item.index === $index) {
-                        result = false;
-                    }
-
-                })
-
-
-            }
-
-            return result;
-
-        };
 
         vm.init = function () {
 
             console.log(' vm.layout', vm.layout);
 
+            vm.updateCanCreateRowStatus();
+            vm.updateProxyAccordions();
 
         };
 
