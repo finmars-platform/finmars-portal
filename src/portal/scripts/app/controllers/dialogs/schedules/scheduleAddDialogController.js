483a74fb3a59db733d1d6f1ed5ef0ac3205d8cf2
vzubr <zubr@rambler.ru>
2021-01-06 09:16:36 +0300

    make draggable pricing schedule procedures

--- src/portal/scripts/app/controllers/dialogs/schedules/scheduleAddDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/schedules/scheduleAddDialogController.js
@@ -9,6 +9,9 @@
     var pricingProcedureService = require('../../../services/procedures/pricingProcedureService');
     var dataProcedureService = require('../../../services/procedures/dataProcedureService');
 
+    const ScrollHelper = require('../../../helpers/scrollHelper');
+    const scrollHelper = new ScrollHelper();
+
     module.exports = function scheduleAddDialogController($scope, $mdDialog, data) {
 
         var vm = this;
@@ -157,10 +160,94 @@
             vm.schedule.procedures = vm.schedule.procedures.map(function (item, index) {
 
                 item.order = index + 1;
+                item.dragOrder = index;
 
                 return item
             })
 
+        };
+
+        vm.dragIconGrabbed = false;
+        vm.dragAndDropInited = false;
+
+        const turnOffDragging = function () {
+            vm.dragIconGrabbed = false;
+        };
+
+        vm.turnOnDragging = function () {
+            vm.dragIconGrabbed = true;
+            document.body.addEventListener('mouseup', turnOffDragging, {once: true});
+        };
+
+        vm.dragAndDrop = {
+            init: function () {
+                this.dragulaInit();
+                this.eventListeners();
+                vm.dragAndDropInited = true;
+            },
+
+            eventListeners: function () {
+                var drake = this.dragula;
+
+                drake.on('drag', function () {
+                    scrollHelper.enableDnDWheelScroll();
+                });
+
+                drake.on('drop', function (elem, target, source, nextSiblings) {
+
+                    var draggedRowOrder = parseInt(elem.dataset.rowOrder);
+                    var siblingRowOrder = null;
+                    if (nextSiblings) {
+                        siblingRowOrder = parseInt(nextSiblings.dataset.rowOrder);
+                    }
+
+                    var rowToInsert = vm.schedule.procedures[draggedRowOrder];
+                    vm.schedule.procedures.splice(draggedRowOrder, 1);
+
+                    if (siblingRowOrder !== null) {
+
+                        for (var i = 0; i < vm.schedule.procedures.length; i++) {
+                            if (vm.schedule.procedures[i].dragOrder === siblingRowOrder) {
+
+                                vm.schedule.procedures.splice(i, 0, rowToInsert);
+                                break;
+
+                            }
+                        }
+
+                    } else {
+                        vm.schedule.procedures.push(rowToInsert);
+                    }
+
+                    for (var i = 0; i < vm.schedule.procedures.length; i++) {
+                        vm.schedule.procedures[i].dragOrder = i;
+                    }
+
+                    $scope.$apply();
+
+                });
+
+                drake.on('dragend', function (elem) {
+                    scrollHelper.disableDnDWheelScroll();
+                });
+            },
+
+            dragulaInit: function () {
+                var items = [
+                    document.querySelector('.pricingSchedulesTableRowsHolder')
+                ];
+
+                this.dragula = dragula(items, {
+                    moves: function () {
+                        if (vm.dragIconGrabbed) {
+                            return true;
+                        }
+
+                        return false;
+                    },
+                    revertOnSpill: true
+                })
+            }
         };
 
         vm.init = function () {
