a7517d6553793d50bbe6d61c4ef9871f3985f85d
vzubr <zubr@rambler.ru>
2021-03-26 10:39:01 +0300

    only active filters must be orange

--- src/portal/scripts/app/directives/groupTable/gFiltersDirective.js
+++ src/portal/scripts/app/directives/groupTable/gFiltersDirective.js
@@ -703,7 +703,8 @@
 							) {
 
 								let filterData = {
-									id: filter.key
+									id: filter.key,
+                                    isActive: filterOpts.enabled
 								};
 
 								const filterName = filter.layout_name ? filter.layout_name : filter.name;
