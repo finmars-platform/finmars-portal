7581ab46c9c798415f41ede655fb40361bcb0b2d
szhitenev <szhitenev>
2021-12-09 19:19:46 +0300

    update instrument typ

--- src/portal/scripts/app/controllers/entityViewer/instrumentTypeEditDialogController.js
+++ src/portal/scripts/app/controllers/entityViewer/instrumentTypeEditDialogController.js
@@ -960,7 +960,7 @@
             if (errors.length) {
 				// vm.sharedLogic.processTabsErrors(errors, $event);
 
-				var processResult = entityEditorHelper.processTabsErrors(errors, vm.evEditorDataService, vm.evEditorEventService, $mdDialog, $event, vm.fixedAreaPopup);
+				var processResult = entityEditorHelper.processTabsErrorsInstrumentType(errors, vm.evEditorDataService, vm.evEditorEventService, $mdDialog, $event, vm.fixedAreaPopup);
 
 				if (processResult) {
 					vm.fixedAreaPopup = processResult;
@@ -1017,6 +1017,31 @@
                 });
 
             }
+
+        };
+
+        vm.saveAndExit = function (action) {
+
+            vm.save().then(function (responseData) {
+
+                let responseObj = {status: 'disagree'};
+
+                if (action === 'edit') {
+
+                    vm.entity = {...vm.entity, ...responseData};
+                    vm.entity.$_isValid = true;
+
+                    responseObj = {
+                        status: 'edit',
+                        data: {
+                            entityType: vm.entityType,
+                            entity: vm.entity
+                        }
+                    };
+                }
+
+                metaHelper.closeComponent(vm.openedIn, $mdDialog, $bigDrawer, responseObj);
+            })
 
         };
 
