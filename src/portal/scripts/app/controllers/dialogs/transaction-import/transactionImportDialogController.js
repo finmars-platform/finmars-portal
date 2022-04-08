49752a7b53f5f4a44735a8a6653ef07c3ce73893
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-04-08 11:26:53 +0400

    fix

--- src/portal/scripts/app/controllers/dialogs/transaction-import/transactionImportDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/transaction-import/transactionImportDialogController.js
@@ -709,6 +709,10 @@ import websocketService from "../../../../../../shell/scripts/app/services/webso
             vm.getSchemeList();
             vm.getMember();
 
+            if (data.scheme) {
+                vm.config.scheme = data.scheme.id
+            }
+
         };
 
         vm.init();
