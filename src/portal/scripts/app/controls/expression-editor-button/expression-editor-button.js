2e47d3fcf4f51efe9c460ee11d3667a66f50ba86
mevstratov <m.evstratov@finmars.com>
2022-06-21 14:32:09 +0300

    dialogs nesting fix for instrument scheme manager

--- src/portal/scripts/app/controls/expression-editor-button/expression-editor-button.js
+++ src/portal/scripts/app/controls/expression-editor-button/expression-editor-button.js
@@ -22,7 +22,7 @@
                     $mdDialog.show({
                         controller: 'ExpressionEditorDialogController as vm',
                         templateUrl: 'views/dialogs/expression-editor-dialog-view.html',
-                        parent: angular.element(document.body),
+                        parent: document.querySelector(".dialog-containers-wrap"),
                         targetEvent: $event,
                         preserveScope: true,
                         multiple: true,
