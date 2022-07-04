15285593ad82227af64430b57b1ad3847a00cbc3
szhitenev <szhitenev>
2022-07-04 23:02:55 +0300

    json editor for entity

--- src/portal/scripts/app/controllers/dialogs/expressionEditorDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/expressionEditorDialogController.js
@@ -964,6 +964,9 @@
                 vm.editor.setBehavioursEnabled(true);
                 vm.editor.setValue(vm.item.expression)
 
+                vm.editor.focus();
+                vm.editor.navigateFileStart();
+
             }, 100)
 
         }
