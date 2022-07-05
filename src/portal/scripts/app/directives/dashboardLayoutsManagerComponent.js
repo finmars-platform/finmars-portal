018b0c954637aa05a2dc134feaf3003cf94ce87c
mevstratov <m.evstratov@finmars.com>
2022-07-05 15:01:31 +0300

    HOTFIX: update date of modification for layout of dashboard

--- src/portal/scripts/app/directives/dashboardLayoutsManagerComponent.js
+++ src/portal/scripts/app/directives/dashboardLayoutsManagerComponent.js
@@ -123,7 +123,7 @@
 					targetLayout.is_default = true;
 
                     uiService.updateDashboardLayout(targetLayout.id, targetLayout).then(async function (data) {
-
+                        scope.dashboardDataService.updateModifiedDate(data.modified);
                         toastNotificationService.success("Dashboard Layout is set as default");
 
                     });
@@ -137,11 +137,12 @@
 
                 scope.saveLayoutList = function () {
 
-                    scope.dashboardEventService.dispatchEvent(popupEvents.CLOSE_POPUP);
-
                     uiService.updateDashboardLayout(scope.layout.id, scope.layout).then(function (data) {
 
                         toastNotificationService.success("Dashboard Layout is Saved")
+                        scope.dashboardDataService.updateModifiedDate(data.modified);
+
+                        scope.dashboardEventService.dispatchEvent(popupEvents.CLOSE_POPUP);
 
 
                     });
@@ -185,7 +186,7 @@
                             scope.layout.name = res.data.name;
 
                             uiService.updateDashboardLayout(scope.layout.id, scope.layout).then(async function (data) {
-
+                                scope.dashboardDataService.updateModifiedDate(data.modified);
                                 toastNotificationService.success("Dashboard Layout is renamed")
 
                             });
