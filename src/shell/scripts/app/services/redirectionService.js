fe83b44ec1b176f57870caa93c817715f8964dd2
Sergey Zhitenev <szhitenev@szhitenev-macbookpro.local>
2022-09-22 15:48:48 +0200

    transaction import json

--- src/shell/scripts/app/services/redirectionService.js
+++ src/shell/scripts/app/services/redirectionService.js
@@ -6,9 +6,15 @@ export default function () {
 
 	const baseUrl = baseUrlService.resolve();
 
+	let profileUrl = baseUrl + '/v/profile'
+
+	if (window.location.href.indexOf('0.0.0.0') !== -1) { // for local develop mode
+		profileUrl = '/#!/profile'
+	}
+
 	const stateToUrl = {
 		'app.portal.home': baseUrl + '/v/',
-		'app.profile': baseUrl + '/v/profile',
+		'app.profile': profileUrl,
 		'app.portal.reports.performance-report': baseUrl + '/v/reports/performance',
 	};
 
