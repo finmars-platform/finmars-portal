ffd60031bbdfc4c0bd09ef492816b1607a4e1992
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-10-16 01:50:48 +0200

    FN-806 init

---
+++ src/portal/scripts/app/controllers/pages/dataStatsPageController.js
@@ -0,0 +1,52 @@
+/**
+ * Created by szhitenev on 15.10.2022.
+ */
+(function () {
+
+    'use strict';
+
+    var baseUrlService = require('../../services/baseUrlService');
+    var dataStatsService = require('../../services/dataStatsService');
+    var downloadFileHelper = require('../../helpers/downloadFileHelper');
+    var toastNotificationService = require('../../../../../core/services/toastNotificationService');
+
+    var baseUrl = baseUrlService.resolve();
+
+
+    module.exports = function dataStatsController($scope, authorizerService, globalDataService, $mdDialog) {
+
+        var vm = this;
+
+        vm.showGeneralSection = true;
+        vm.showPriceHistorySection = false;
+        vm.showCurrencyHistorySection = false;
+        vm.showNavHistorySection = false;
+        vm.showPlHistorySection = false;
+        vm.showWidgetStatsSection = false;
+
+        vm.showInstruments = false;
+        vm.showCurrencies = false;
+
+        vm.readyStatus = {content: false};
+
+        vm.init = function () {
+
+            dataStatsService.getStats().then(function (data){
+
+                vm.stats = data;
+
+                console.log('vm.stats', vm.stats);
+
+                vm.readyStatus.content = true;
+
+                $scope.$apply();
+
+            })
+
+        };
+
+        vm.init();
+
+    };
+
+}());
