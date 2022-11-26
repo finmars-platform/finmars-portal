e5b7af5bf2fea10e1c84debe6dc44b148f5b0c90
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2022-11-26 12:18:42 +0100

    fix homepage link

--- src/shell/scripts/app/services/redirectionService.js
+++ src/shell/scripts/app/services/redirectionService.js
@@ -4,22 +4,25 @@ import baseUrlService from "./baseUrlService";
 
 export default function () {
 
-	const baseUrl = baseUrlService.resolve();
-	const base_api_url = baseUrlService.getMasterUserPrefix()
 
-	let profileUrl = baseUrl + '/v/profile'
-
-	if (window.location.href.indexOf('0.0.0.0') !== -1) { // for local develop mode
-		profileUrl = '/#!/profile'
-	}
-
-	const stateToUrl = {
-		'app.portal.home': baseUrl + '/' + base_api_url + '/v/',
-		'app.profile': profileUrl,
-		'app.portal.reports.performance-report': baseUrl + '/' + base_api_url + '/v/reports/performance',
-	};
 
 	function getUrl (stateName) {
+
+		const baseUrl = baseUrlService.resolve();
+		const base_api_url = baseUrlService.getMasterUserPrefix()
+
+		let profileUrl = baseUrl + '/v/profile'
+
+		if (window.location.href.indexOf('0.0.0.0') !== -1) { // for local develop mode
+			profileUrl = '/#!/profile'
+		}
+
+		const stateToUrl = {
+			'app.portal.home': baseUrl + '/' + base_api_url + '/v/',
+			'app.profile': profileUrl,
+			'app.portal.reports.performance-report': baseUrl + '/' + base_api_url + '/v/reports/performance',
+		};
+
 
 		if (!stateToUrl.hasOwnProperty(stateName)) {
 			throw new Error('There is no state with such name')
