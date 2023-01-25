8653aad31065d1dc522c02942bc198fe54b22421
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2023-01-25 19:29:21 +0100

    update preprocess

--- src/portal/scripts/app/controllers/pages/transactionImportController.js
+++ src/portal/scripts/app/controllers/pages/transactionImportController.js
@@ -851,6 +851,7 @@ import importTransactionService from "../../services/import/importTransactionSer
 
                     formData.append('file', blob, 'input.json');
                     formData.append('scheme', vm.config.scheme);
+                    formData.append('preprocess_file', vm.config.preprocess_file);
 
                     vm.fileLocal = vm.config.local;
 
@@ -864,6 +865,7 @@ import importTransactionService from "../../services/import/importTransactionSer
 
                     formData.append('file', vm.config.file);
                     formData.append('scheme', vm.config.scheme);
+                    formData.append('preprocess_file', vm.config.preprocess_file);
 
                     vm.fileLocal = vm.config.local;
 
