995cd94bc81cec4462f4122dccb2500094943bfa
mevstratov <m.evstratov@finmars.com>
2023-01-29 19:38:25 +0400

    FN-1008 - fixed

--- src/portal/scripts/app/models/instrumentTypePropsModel.js
+++ src/portal/scripts/app/models/instrumentTypePropsModel.js
@@ -107,6 +107,73 @@
                 "name": "Object permissions",
                 "value_type": "mc_field"
             },
+
+            //# region Exposure
+            {
+                "key": "underlying_long_multiplier",
+                "name": "Underlying long multiplier",
+                "value_type": 20
+            },
+            {
+                "key": "underlying_short_multiplier",
+                "name": "Underlying short multiplier",
+                "value_type": 20
+            },
+
+            {
+                "key": "co_directional_exposure_currency",
+                "name": "Exposure Co-Directional Currency",
+                "value_content_type": "currencies.currency",
+                "value_entity": "currency",
+                "code": "user_code",
+                "value_type": "field"
+            },
+            {
+                "key": "counter_directional_exposure_currency",
+                "name": "Exposure Counter-Directional Currency",
+                "value_content_type": "currencies.currency",
+                "value_entity": "currency",
+                "code": "user_code",
+                "value_type": "field"
+            },
+            {
+                "key": "long_underlying_exposure",
+                "name": "Long Underlying Exposure",
+                "value_content_type": "instruments.longunderlyingexposure",
+                "value_entity": "long-underlying-exposure",
+                "value_type": "field"
+            },
+            {
+                "key": "short_underlying_exposure",
+                "name": "Short Underlying Exposure",
+                "value_content_type": "instruments.shortunderlyingexposure",
+                "value_entity": "short-underlying-exposure",
+                "value_type": "field"
+            },
+            {
+                "key": "exposure_calculation_model",
+                "name": "Exposure Calculation Model",
+                "value_content_type": "instruments.exposurecalculationmodel",
+                "value_entity": "exposure-calculation-model",
+                "value_type": "field"
+            },
+
+            {
+                "key": "long_underlying_instrument",
+                "name": "Long Underlying Instrument",
+                "value_content_type": "instruments.instrument",
+                "value_entity": "instrument",
+                "value_type": "field"
+            },
+            {
+                "key": "short_underlying_instrument",
+                "name": "Short Underlying Instrument",
+                "value_content_type": "instruments.instrument",
+                "value_entity": "instrument",
+                "value_type": "field"
+            },
+            //# endregion
+
 			{
 				"key": "accrued_currency",
 				"name": "Accrued currency",
