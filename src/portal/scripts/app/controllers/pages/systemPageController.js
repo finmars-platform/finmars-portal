1475c69a8926121d260cd07b29947adfba03d690
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2023-02-02 16:30:49 +0100

    base logic of system page

--- src/portal/scripts/app/controllers/pages/systemPageController.js
+++ src/portal/scripts/app/controllers/pages/systemPageController.js
@@ -9,6 +9,7 @@
 
     var baseUrlService = require('../../services/baseUrlService');
     var utilsService = require('../../services/utilsService');
+    var downloadFileHelper = require('../../helpers/downloadFileHelper');
 
     var baseUrl = baseUrlService.resolve();
 
@@ -19,21 +20,82 @@
 
         vm.processing = false;
 
-        vm.getData = function () {
+        vm.readyStatus = {
+            stats: false,
+            logs: false
+        }
 
-            vm.processing = true;
+
+        vm.getStats = function () {
+
+            vm.readyStatus.stats = false;
 
             utilsService.getSystemInfo().then(function (data) {
-                vm.systemInfo = data;
-                vm.processing = false;
+                vm.systemInfoItems = data.results;
+                vm.readyStatus.stats = true;
                 $scope.$apply();
+            })
+
+        }
+
+        vm.getLogs = function () {
+
+            vm.readyStatus.logs = false;
+
+            utilsService.getSystemLogs().then(function (data) {
+
+                vm.logFiles = data.results;
+
+                vm.readyStatus.logs = true;
+                $scope.$apply()
+
+            })
+
+        }
+
+        vm.previewLog = function ($event, log_file_name) {
+
+            utilsService.getSystemLog(log_file_name).then(function (data) {
+
+                $mdDialog.show({
+                    controller: 'FilePreviewDialogController as vm',
+                    templateUrl: 'views/dialogs/file-preview-dialog-view.html',
+                    parent: angular.element(document.body),
+                    targetEvent: $event,
+                    clickOutsideToClose: false,
+                    preserveScope: true,
+                    autoWrap: true,
+                    skipHide: true,
+                    multiple: true,
+                    locals: {
+                        data: {
+                            content: data,
+                            file_descriptor: {
+                                name: log_file_name
+                            }
+                        }
+                    }
+                });
+
+
+            })
+
+        }
+
+        vm.downloadLog = function ($event, log_file_name) {
+
+            utilsService.getSystemLog(log_file_name).then(function (data) {
+
+                downloadFileHelper.downloadFile(data, "plain/text", log_file_name);
+
             })
 
         }
 
         vm.init = function () {
 
-            vm.getData();
+            vm.getStats();
+            vm.getLogs()
 
 
         };
