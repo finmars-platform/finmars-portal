773542bd6c476c443ad45864aad20195a71c4f36
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-03-08 19:05:18 +0100

    add show data button to journal

--- src/portal/scripts/app/services/historyService.js
+++ src/portal/scripts/app/services/historyService.js
@@ -16,7 +16,7 @@
         var apiVersion = baseUrlService.getApiVersion();
 
 
-        return xhrService.fetch(configureRepositoryUrlService.configureUrl(baseUrl  +  '/' + prefix + '/' + apiVersion + '/history/historical-record/', options),
+        return xhrService.fetch(configureRepositoryUrlService.configureUrl(baseUrl + '/' + prefix + '/' + apiVersion + '/history/historical-record/', options),
             {
                 method: 'GET',
                 credentials: 'include',
@@ -48,10 +48,28 @@
             })
     };
 
+    var getRecordData = function (id) {
+
+        var prefix = baseUrlService.getMasterUserPrefix();
+        var apiVersion = baseUrlService.getApiVersion();
+
+        return xhrService.fetch(baseUrl + '/' + prefix + '/' + apiVersion + '/history/historical-record/' + id + '/data/',
+            {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'X-CSRFToken': cookieService.getCookie('csrftoken'),
+                    'Authorization': 'Token ' + cookieService.getCookie('access_token'),
+                    Accept: 'application/json',
+                    'Content-type': 'application/json'
+                }
+            })
+    };
 
     module.exports = {
         getList: getList,
-        getByKey: getByKey
+        getByKey: getByKey,
+        getRecordData: getRecordData
     }
 
 }());
