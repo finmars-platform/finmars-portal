7a5e1c1d077497dcc9a8b6f9ecd8e26a1459a5b0
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2023-03-12 21:13:44 +0100

    update keycloak integration

--- src/core/services/axService.js
+++ src/core/services/axService.js
@@ -32,32 +32,55 @@ import cookieService from "./cookieService";
                 })
             }
 
-            // error on login
-            if (err.response.config.url.indexOf('/token-auth/') !== -1) {
-                return new Promise((resolve, reject) => {
-                    reject(err)
-                })
-            }
-            // error on refresh
-            if (err.response.config.url.indexOf('/token-refresh/') !== -1) {
+            return window.keycloak.updateToken().then(function (){
 
+                const config = err.response.config
+                config.headers.Authorization = 'Token ' + window.keycloak.token
 
-                cookieService.deleteCookie('access_token')
-                cookieService.deleteCookie('refresh_token')
-                window.location.reload();
-                return new Promise((resolve, reject) => {
-                    reject(err)
+                cookieService.setCookie('access_token', window.keycloak.token);
+                cookieService.setCookie('refresh_token', window.keycloak.refreshToken);
+                cookieService.setCookie('id_token', window.keycloak.idToken);
+
+                return ax(config)
+
+            }).catch(function(error) {
+
+                console.log("Keycloak update error", error)
+
+                // in case if refresh token is expired
+
+                window.keycloak.init({
+                    onLoad: 'login-required'
                 })
-            }
-            // refresh
-            return ax.post(authorizerUrl + '/token-refresh/', {refresh_token: cookieService.getCookie('refresh_token')}, {withCredentials: true}).then(
-                response => {
-                    const config = err.response.config
-                    config.headers.Authorization = 'Token ' + response.data.access_token
-                    cookieService.setCookie('access_token', response.data.access_token)
-                    return ax(config)
-                }
-            )
+
+            });
+
+            // // error on login
+            // if (err.response.config.url.indexOf('/token-auth/') !== -1) {
+            //     return new Promise((resolve, reject) => {
+            //         reject(err)
+            //     })
+            // }
+            // // error on refresh
+            // if (err.response.config.url.indexOf('/token-refresh/') !== -1) {
+            //
+            //
+            //     cookieService.deleteCookie('access_token')
+            //     cookieService.deleteCookie('refresh_token')
+            //     window.location.reload();
+            //     return new Promise((resolve, reject) => {
+            //         reject(err)
+            //     })
+            // }
+            // // refresh
+            // return ax.post(authorizerUrl + '/token-refresh/', {refresh_token: cookieService.getCookie('refresh_token')}, {withCredentials: true}).then(
+            //     response => {
+            //         const config = err.response.config
+            //         config.headers.Authorization = 'Token ' + response.data.access_token
+            //         cookieService.setCookie('access_token', response.data.access_token)
+            //         return ax(config)
+            //     }
+            // )
         }
     )
 
