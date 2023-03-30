61422abf1ee73120fdd54f425402dbc06a3bb066
Sergey Zhitenev <szhitenev@ip-192-168-178-30.eu-central-1.compute.internal>
2023-03-30 16:06:03 +0200

    universal input complex transaction

--- src/portal/scripts/app/controllers/dialogs/entityAsJsonEditorDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/entityAsJsonEditorDialogController.js
@@ -24,67 +24,100 @@
 
             Object.keys(obj).forEach(function (key) {
 
-                if (key === 'id') {
-                    delete obj.id
-                }
+                if (vm.data.entityType === 'complex-transaction') {
 
-                if (key === 'deleted_user_code') {
-                    delete obj.deleted_user_code
-                }
+                    if (['inputs', 'transaction_type', 'status', 'meta', 'source'].indexOf(key) === -1) {
 
-                if (key === 'is_deleted') {
-                    delete obj.is_deleted
-                }
-
-                if (obj.hasOwnProperty(key + '_object')) {
-
-                    if (obj[key + '_object']) {
-
-                        obj[key] = obj[key + '_object']['user_code']
-                        delete obj[key + '_object']
-                    }
-                }
-
-
-                if (key === 'pricing_policies') {
-                    delete obj.pricing_policies
-                }
-                if (key === 'registers') {
-                    delete obj.registers
-                }
-
-                if (key === 'attributes') {
-
-                    obj.attributes = obj.attributes.map(function (attribute) {
-
-                        attribute.attribute_type = attribute.attribute_type_object.user_code
-                        delete attribute.attribute_type_object;
-
-                        if (attribute.classifier_object) {
-                            attribute.classifier = attribute.classifier_object.name
-                            delete attribute.classifier_object;
+                        if (key === 'transaction_type_object') {
+                            obj.transaction_type = obj.transaction_type_object.user_code;
                         }
 
-                        return attribute
-                    })
+                        delete obj[key]
+                    }
+
+                    if (key === 'inputs') {
+
+                        obj.inputs = obj.inputs.map(function (input){
+
+                            input.transaction_type_input = input.transaction_type_input_object.name
+                            delete input.transaction_type_input_object;
+
+                            return input;
+
+                        })
+
+                    }
+
+
+                } else {
+
+                    if (key === 'id') {
+                        delete obj.id
+                    }
+
+                    if (key === 'deleted_user_code') {
+                        delete obj.deleted_user_code
+                    }
+
+                    if (key === 'is_deleted') {
+                        delete obj.is_deleted
+                    }
+
+                    if (obj.hasOwnProperty(key + '_object')) {
+
+                        if (obj[key + '_object']) {
+
+                            obj[key] = obj[key + '_object']['user_code']
+                            delete obj[key + '_object']
+                        }
+                    }
+
+
+                    if (key === 'pricing_policies') {
+                        delete obj.pricing_policies
+                    }
+                    if (key === 'registers') {
+                        delete obj.registers
+                    }
+
+
+
+                    if (key === 'attributes') {
+
+                        obj.attributes = obj.attributes.map(function (attribute) {
+
+                            attribute.attribute_type = attribute.attribute_type_object.user_code
+                            delete attribute.attribute_type_object;
+
+                            if (attribute.classifier_object) {
+                                attribute.classifier = attribute.classifier_object.name
+                                delete attribute.classifier_object;
+                            }
+
+                            return attribute
+                        })
+
+                    }
+
+                    if (key === 'accrual_calculation_schedules') {
+
+                        obj.accrual_calculation_schedules = obj.accrual_calculation_schedules.map(function (item) {
+
+                            delete item['id']
+                            item['accrual_calculation_model'] = item['accrual_calculation_model_object']['user_code']
+                            delete item['accrual_calculation_model_object']
+
+                            item['periodicity'] = item['periodicity_object']['user_code']
+                            delete item['periodicity_object']
+
+                            return item
+                        })
+
+                    }
 
                 }
 
-                if (key === 'accrual_calculation_schedules') {
 
-                    obj.accrual_calculation_schedules = obj.accrual_calculation_schedules.map(function (item) {
-
-                        delete item['id']
-                        item['accrual_calculation_model'] = item['accrual_calculation_model_object']['user_code']
-                        delete item['accrual_calculation_model_object']
-
-                        item['periodicity'] = item['periodicity_object']['user_code']
-                        delete item['periodicity_object']
-
-                        return item
-                    })
-
-                }
 
 
             })
@@ -126,6 +159,8 @@
         }
 
         vm.init = function () {
+
+            console.log('vm.data', vm.data);
 
             setTimeout(function () {
                 vm.dialogElemToResize = document.querySelector('.entityAsJsonEditorDialogElemToResize');
