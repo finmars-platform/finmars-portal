6c1580d8d9600b97ac19cab9c065e0550f23bf97
Sergei Zhitenev <s.zhitenev@finmars.com>
2023-05-22 14:26:53 +0200

    fix multiselect

--- src/portal/scripts/app/directives/dashboard/dashboardControlDirective.js
+++ src/portal/scripts/app/directives/dashboard/dashboardControlDirective.js
@@ -167,6 +167,8 @@
 
                                 scope.item.data.store.value = getSelectedIds(scope.item.data.store.user_codes);
 
+                            } else {
+                                scope.item.data.store.value = [];
                             }
 
 
@@ -214,18 +216,8 @@
                         } else {
                             changedData.data = JSON.parse(JSON.stringify(scope.item.data.store));
                         }
-                    } else {
-
-                        scope.componentData.value = null;
-                        var emptyVal = scope.componentData.settings.multiple ? [] : null;
-
-                        scope.entityData.value = null; // for entity search select
-
-                        scope.item.data.store.value = emptyVal
-                        scope.item.data.store.name = '';
-                        scope.item.data.store.user_codes = emptyVal;
-
                     }
+
 
                     console.log('valueChanged.value before output', scope.item.data.store.value);
 
