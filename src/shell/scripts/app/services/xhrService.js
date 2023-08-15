9e973f8b4a71c7a4a6044ee56be83dfdb85d1db5
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2023-08-15 12:47:34 +0200

    FN-2008 fix xhr

--- src/shell/scripts/app/services/xhrService.js
+++ src/shell/scripts/app/services/xhrService.js
@@ -1,5 +1,6 @@
 /**
  * Created by mevstratov on 25.05.2021.
+ * Updated by szhitenev on 15.08.2023.
  */
 
 'use strict';
@@ -7,7 +8,6 @@
 
 export default function (errorService, cookieService) {
 
-    // var axService = require('../../../../core/services/axService')
     const getRequestParams = function (method, bodyData) {
 
         if (!['GET', 'POST', 'PATCH', 'PUT', 'DELETE'].includes(method)) {
@@ -43,234 +43,100 @@ export default function (errorService, cookieService) {
         try {
             return await response;
 
-        } catch (error) { throw error; }
+        } catch (error) {
+            throw error;
+        }
 
     }
 
-    const axfetch = function (url, params, options) {
+    async function refreshToken() {
 
-        let requestId;
-        const notifyError = options && options.hasOwnProperty('notifyError') ? options.notifyError : true;
+        return new Promise(function (resolve, reject) {
 
-        if (window.developerConsoleService) {
-            requestId = window.developerConsoleService.pushRequest({
-                url: url,
-                params: params
+            window.keycloak.updateToken().then(function () {
+
+
+                cookieService.setCookie('access_token', window.keycloak.token);
+                cookieService.setCookie('refresh_token', window.keycloak.refreshToken);
+                cookieService.setCookie('id_token', window.keycloak.idToken);
+
+                resolve(window.keycloak.token);
+
             })
+
+        })
+
+    }
+
+
+    const fetch = async function (url, params = {}, options = {}) {
+
+        const {notifyError = true} = options;
+
+        try {
+            let response = await window.fetch(url, params);
+
+            // If a 401 is encountered, try to refresh the token and retry the request
+            if (response.status === 401) {
+
+                console.log("Catch Authorization error, trying to refresh token")
+
+                const newToken = await refreshToken();
+
+                // Update the headers with the new token and retry the request
+                params.headers = {
+                    ...params.headers,
+                    Authorization: `Token ${newToken}`
+                };
+
+                response = await window.fetch(url, params);
+            }
+
+            console.log('response', response);
+
+            // Handle the refreshed response
+            if (response.status === 204) return response;
+
+            if (response.status === 404) {
+                throw buildError(response, {
+                    message: response.statusText,
+                    status_code: response.status,
+                    url: response.url,
+                    username: '',
+                    datetime: '',
+                    details: '',
+                });
+            }
+
+            if (response.status >= 400 && response.status < 600 && response.status !== 401) {
+                const errorData = await response.json();
+                throw buildError(response, errorData);
+            }
+
+            return params.method !== "DELETE" ? await response.json() : response;
         }
+        catch (error) {
+            console.log('xhrService.reason', error);
 
-        params.url = url
+            if (notifyError) {
+                await errorService.notifyError(error);
+            }
 
-        return axService.ax.request(params)
-            .then(function (response) {
-
-                return new Promise(function (resolve, reject) {
-
-                    if (window.developerConsoleService) {
-                        window.developerConsoleService.resolveRequest(requestId, response.clone())
-                    }
-
-                    if (response.status === 204) { // No content
-                        resolve(response);
-                    } else if (response.status >= 400 && response.status < 500) {
-
-                        response.json().then(function (data) {
-
-                            const error = {
-                                status: response.status,
-                                statusText: response.statusText,
-                                message: data
-                            };
-
-                            reject(error)
-
-                        })
-
-                    } else if (response.status >= 500 && response.status < 600) {
-
-                        const error = {
-                            status: response.status,
-                            statusText: response.statusText,
-                            message: response.statusText
-                        };
-
-                        reject(error)
-
-                    } else {
-
-                        if (params.method !== "DELETE") {
-                            resolve(response.data);
-                        } else {
-                            resolve(response);
-                        }
-
-
-                    }
-
-                })
-            })
-            .catch(function (reason) {
-
-                console.log('xhrService.reason', reason);
-
-                /*if (window.developerConsoleService) {
-                    window.developerConsoleService.rejectRequest(requestId, reason)
-                }*/
-
-                if (notifyError !== false) {
-                    errorService.notifyError(reason);
-                }
-
-                console.log('XHR Service catch error', reason);
-
-                throw reason;
-
-            })
-
+            console.error('XHR Service catch error', error);
+            throw error;
+        }
     };
 
-    const fetch = function (url, params, options) {
+    function buildError(response, errorData) {
+        return {
+            error: {
+                ...errorData,
+                status_code: response.status,
+                url: response.url
+            }
+        };
+    }
 
-        let requestId;
-        const notifyError = options && options.hasOwnProperty('notifyError') ? options.notifyError : true;
-
-        // Deprecated
-        // if (window.developerConsoleService) {
-        // 	requestId = window.developerConsoleService.pushRequest({
-        // 		url: url,
-        // 		params: params
-        // 	})
-        // }
-
-        return window
-            .fetch(url, params)
-            .then(async function (response) {
-
-                if (response.status === 204) { // No content
-                    return response;
-
-                }
-                else if (response.status === 404) {
-
-                    throw {
-                        error: {
-                            message: response.statusText,
-                            status_code: response.status,
-                            url: response.url,
-                            username: '',
-                            datetime: '',
-                            details: '',
-                        }
-                    };
-                }
-                else if (response.status >= 400 && response.status < 500) {
-
-                    /*response.json().then(function (data) {
-
-                        const error = {
-                            status: response.status,
-                            statusText: response.statusText,
-                            message: data
-                        };
-
-                        reject(error)
-
-                    })*/
-
-                    throw await response.json();
-
-                } else if (response.status >= 500 && response.status < 600) {
-
-                    const error = await response.json();
-                    throw error;
-
-                } else {
-
-                    if (params.method !== "DELETE") {
-                        return response.json();
-                    } else {
-                        return response;
-                    }
-
-                }
-
-                // })
-            })
-            .catch(async function (reason) {
-
-                console.log('xhrService.reason', reason)
-
-                if (notifyError !== false) {
-                    await errorService.notifyError(reason);
-                }
-
-                // Deprecated
-                // if (reason.status !== 401 && (reason.error && reason.error.status_code !== 401)) {
-                //
-                //     if (url.includes('/token-refresh/') &&
-                //         !url.includes('/token-auth/')) {
-                //
-                //         cookieService.deleteCookie('access_token')
-                //         cookieService.deleteCookie('refresh_token')
-                //         window.location.reload();
-                //
-                //         throw reason;
-                //
-                //     }
-                //
-                //
-                // }
-                // else {
-                //
-                //     try {
-                //
-                //         const res = await window.keycloak.updateToken()
-                //         console.log('res', res)
-                //
-                //         if (res) {
-                //
-                //             cookieService.setCookie('access_token', window.keycloak.token);
-                //             cookieService.setCookie('refresh_token', window.keycloak.refreshToken);
-                //             cookieService.setCookie('id_token', window.keycloak.idToken);
-                //
-                //             throw null;
-                //             // return fetch(url, params, options); // try to request again with refreshed token
-                //         } else {
-                //             window.keycloak.init({
-                //                 onLoad: 'login-required'
-                //             })
-                //         }
-                //
-                //     }
-                //     catch (error) {
-                //
-                //         error.___custom_message = 'Keycloak update error';
-                //         console.error(error)
-                //
-                //         // in case if refresh token is expired
-                //
-                //         window.keycloak.init({
-                //             onLoad: 'login-required'
-                //         })
-                //
-                //         throw null;
-                //
-                //     }
-                //
-                // }
-                //
-                // if (notifyError !== false) {
-                //     await errorService.notifyError(reason);
-                // }
-
-
-                console.error('XHR Service catch error', reason);
-
-                throw reason;
-
-            })
-
-    };
 
     return {
         fetch: fetch,
