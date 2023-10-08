20ab8433cc8b23bcb8c85c4270840f648cd0800d
mevstratov <m.evstratov@finmars.com>
2023-10-08 22:55:34 +0400

    FN-2187 - bug 2 fix 1

--- src/portal/scripts/app/services/entityViewerDataService.js
+++ src/portal/scripts/app/services/entityViewerDataService.js
@@ -1391,6 +1391,20 @@
                     }
                 }
 
+                listLayout.data.grouping = listLayout.data.grouping.map( groupType => {
+
+                    if (!groupType.report_settings) {
+                        groupType.report_settings = {};
+                    }
+
+                    if (typeof groupType.report_settings.is_level_folded !== 'boolean') {
+                        groupType.report_settings.is_level_folded = true;
+                    }
+
+                    return groupType;
+
+                })
+
                 listLayout.data.filters = emptyUseFromAboveFilters(listLayout.data.filters);
 
             }
