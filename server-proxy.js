f1c03aa7ef4ab889c46473a4b26ebe4c326c3ca6
mevstratov <m.evstratov@finmars.com>
2024-04-07 23:08:03 +0400

    PLAT-556 - server proxy uses realms

--- server-proxy.js
+++ server-proxy.js
@@ -10,11 +10,23 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 var proxyApiHost = process.env.API_HOST || 'https://stage.finmars.com';
-var proxyDatabaseId = process.env.DATABASE_ID || 'space00000';
+var proxyRealm = process.env.REALM;
+var proxyDatabaseId = process.env.DATABASE_ID;
+
+if (!proxyDatabaseId) {
+    console.error("DATABASE_ID is not set")
+}
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/portal', express.static('dist'));
-app.use('/' + proxyDatabaseId + '/a', express.static('dist'));
+
+let realmAndSpace = `/${proxyDatabaseId}/a`;
+
+if (proxyRealm) {
+    realmAndSpace = `/${proxyRealm}${realmAndSpace}`;
+}
+
+app.use(realmAndSpace, express.static('dist'));
 
 var proxyOptions = url.parse(proxyApiHost);
 
@@ -25,5 +37,6 @@ proxyOptions.headers = {Referer: proxyApiHost};
 app.use('/', proxy(proxyOptions));
 
 app.listen(port, function () {
+    console.info(`REALM: ${proxyRealm}; DATABASE_ID: ${proxyDatabaseId}`);
     console.info('Express server with proxy start at ' + port + ' port');
 });
