024757b030559c9e66fe960a7785d4b11327bdd6
mevstratov <m.evstratov@finmars.com>
2024-09-16 15:50:15 +0400

    PLAT-769 - integrate tailwindcss, integrate finmars ui, register vue components as web components

---
+++ src/vue/registerComponents.js
@@ -0,0 +1,11 @@
+import {defineCustomElement} from "vue";
+import {FmBtn} from "@finmars/ui";
+
+export default function registerVueComponents () {
+    /*
+    // Example of creating a WEB component based on the VUE component FmBtn
+    const FmBtnCustomElem = defineCustomElement(FmBtn, {shadowRoot: false});
+
+    customElements.define('fm-btn', FmBtnCustomElem);
+    */
+}
