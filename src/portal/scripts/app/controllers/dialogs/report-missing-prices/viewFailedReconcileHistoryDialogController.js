407b378a7ae16ce4e1d97a25c67a2122f1c60f89
akhachatryan <a.khachatryan@finmars.com>
2025-03-03 18:56:16 +0400

    PLAT-1629 - Add fixing on reconcile

--- src/portal/scripts/app/controllers/dialogs/report-missing-prices/viewFailedReconcileHistoryDialogController.js
+++ src/portal/scripts/app/controllers/dialogs/report-missing-prices/viewFailedReconcileHistoryDialogController.js
@@ -11,6 +11,33 @@
 
         vm.items = data.results;
 
+        vm.getStatusTitle = function(status) {
+            let result = {};
+            switch(status) {
+                case 'error':
+                    result.text = 'Error';
+                    result.color = 'var(--error-color)';
+                    break;
+                case 'not_run_yet':
+                    result.text = 'Not run yet';
+                    result.color = 'var(--primary-color)';
+                    break;
+                case 'no_group':
+                    result.text = 'No group';
+                    result.color = 'var(--primary-color)';
+                    break;
+                case 'ok':
+                    result.text = 'Ok';
+                    result.color = 'var(--success-color)';
+                    break;
+                default:
+                    result.text = status;
+                    result.color = 'var(--primary-color)';
+                    break;
+            }
+            return result;
+        };
+
         vm.agree = function () {
             $mdDialog.hide({status: 'agree'});
         };
